e283c5b6170059ebb646827b732cedd75854f0b2
Keltin Mesonjesi <keltin.mesonjesi@protonmail.ch>
2020-10-05 14:28:12 +0200

    Dynamically get record types

--- force-app/main/default/lwc/hdtTargetObjectRecordTypeSelection/hdtTargetObjectRecordTypeSelection.js
+++ force-app/main/default/lwc/hdtTargetObjectRecordTypeSelection/hdtTargetObjectRecordTypeSelection.js
@@ -1,50 +1,57 @@
-import { LightningElement, wire } from 'lwc';
-// import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-// import SERVICE_POINT_OBJECT from '@salesforce/schema/ServicePoint__c';
+import { LightningElement, wire, track } from 'lwc';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import SERVICE_POINT_OBJECT from '@salesforce/schema/ServicePoint__c';
 
 export default class HdtTargetObjectRecordTypeSelection extends LightningElement {
 
-    // @wire(getObjectInfo, { objectApiName: SERVICE_POINT_OBJECT })
-    // objectInfo;
+    @wire(getObjectInfo, { objectApiName: SERVICE_POINT_OBJECT })
+    objectInfo;
 
-    // getRecordTypes(){
-    //     let recordTypeInfos = this.objectInfo.data.recordTypeInfos;
-    //     console.log('OK! ---- Record types: ', JSON.stringify(recordTypeInfos));
+    @track recordType = {label:'',value: ''};
 
-    //     var recordTypeOptions = [];
-    //     for(var eachRecordtype in  recordTypeInfos)
-    //     {
-    //         if(recordTypeInfos[eachRecordtype].name !== 'Master') {
-    //             recordTypeOptions.push(
-    //                 { 
-    //                     label: recordTypeInfos[eachRecordtype].name,
-    //                     value: recordTypeInfos[eachRecordtype].recordTypeId 
-    //                 }
-    //             );
-    //         }
+    get defaultRecordType(){
+        return {label:'',value: this.objectInfo.data.defaultRecordTypeId};
+    }
+
+    get recordTypeOptions(){
+        
+        let recordTypeOptions = [];
+
+        let recordTypeInfos = this.objectInfo.data.recordTypeInfos;
+
+        for(var eachRecordtype in  recordTypeInfos)
+        {
+            if(recordTypeInfos[eachRecordtype].name !== 'Master') {
+                recordTypeOptions.push(
+                    { 
+                        label: recordTypeInfos[eachRecordtype].name,
+                        value: recordTypeInfos[eachRecordtype].recordTypeId 
+                    }
+                );
+            }
             
-    //     }
+        }
 
-    //     return recordTypeOptions;
-    // }
-
-    recordTypeOptions = [
-        { label: 'Elettrico', value: 'elettrico' },
-        { label: 'Gas', value: 'gas' },
-    ];
-    recordType = this.recordTypeOptions[0];
+        return recordTypeOptions;
+    }
 
     closeRecordTypeSelectionModal(){
         this.dispatchEvent(new CustomEvent('closerecordtypeselection'));
     }
 
     handleRecordTypeSelection(event){
-        // this.recordType = event.target.value;
-        this.recordType = this.recordTypeOptions[0];
-        console.log(this.recordTypeOptions.filter(obj => obj.value == event.target.value));
+        let selectedRecordType = this.recordTypeOptions.filter(obj => obj.value == event.target.value);
+        this.recordType = selectedRecordType[0];
+        console.log(selectedRecordType[0]);
     }
 
     next(){
+
+        if(this.recordType.value === ''){
+            let selectedRecordType = this.recordTypeOptions.filter(obj => obj.value == this.defaultRecordType.value);
+            this.recordType = selectedRecordType[0];
+        }
+
         this.dispatchEvent(new CustomEvent('next', {detail:this.recordType}));
     }
 
