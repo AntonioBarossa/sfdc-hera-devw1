506e34d4594c137509c91f5b11d2f5153d6c2e90
Keltin Mesonjesi <keltin.mesonjesi@protonmail.ch>
2020-10-06 10:19:28 +0200

    started create form

--- force-app/main/default/lwc/hdtTargetObjectCreateForm/hdtTargetObjectCreateForm.js
+++ force-app/main/default/lwc/hdtTargetObjectCreateForm/hdtTargetObjectCreateForm.js
@@ -1,9 +1,20 @@
 import { LightningElement, api } from 'lwc';
+import getCustomSettings from '@salesforce/apex/HDT_LC_ServicePointCustomSettings.getCustomSettings';
 
 export default class HdtTargetObjectCreateForm extends LightningElement {
     @api recordtype;
 
+    connectedCallback(){
+        console.log(this.recordtype);
+        getCustomSettings().then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
     closeCreateTargetObjectModal(){
         this.dispatchEvent(new CustomEvent('closecreateform'));
     }
+
 }
