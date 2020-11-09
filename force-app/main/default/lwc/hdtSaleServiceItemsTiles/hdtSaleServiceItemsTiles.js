6105f2a529c9b7b65c93d51c1da17ff295c6c14f
Keltin Mesonjesi <59123033+keltinMesonjesi@users.noreply.github.com>
2020-11-09 15:16:17 +0100

    Delete Opportunity tile

--- force-app/main/default/lwc/hdtSaleServiceItemsTiles/hdtSaleServiceItemsTiles.js
+++ force-app/main/default/lwc/hdtSaleServiceItemsTiles/hdtSaleServiceItemsTiles.js
@@ -1,16 +1,27 @@
 import { LightningElement, api, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getTiles from '@salesforce/apex/HDT_LC_SaleServiceItemsTiles.getTiles';
+import deleteTile from '@salesforce/apex/HDT_LC_SaleServiceItemsTiles.deleteTile';
 
 export default class hdtSaleServiceItemsTiles extends LightningElement {
 
     @api sale;
     @track tilesData;
     showTilesInList = false;
+    loaded = false;
+    isDialogVisible = false;
+    opportunityToDeleteId;
+    opportunityToDeleteName;
+    dialogTitle;
+    dialogMessage;
 
     @api
     getTilesData(){
         getTiles({saleId:this.sale.Id}).then(data =>{
+
+            this.loaded = true;
+
+            console.log(JSON.parse(JSON.stringify(data)));
             
             let tilesArray = [];
 
@@ -29,6 +40,7 @@ export default class hdtSaleServiceItemsTiles extends LightningElement {
             this.showTilesInList = data.length > 4 ? true : false;
 
         }).catch(error => {
+            this.loaded = true;
             const toastErrorMessage = new ShowToastEvent({
                 title: 'Errore',
                 message: error.message,
@@ -40,6 +52,44 @@ export default class hdtSaleServiceItemsTiles extends LightningElement {
 
     connectedCallback(){
         this.getTilesData();
+    }
+
+    handleTileDelete(event){
+        this.opportunityToDeleteId = event.currentTarget.dataset.id;
+        this.opportunityToDeleteName = event.currentTarget.dataset.name;
+        this.dialogTitle = 'Cancella '+ this.opportunityToDeleteName;
+        this.dialogMessage = 'Vuoi cancellare ' + this.opportunityToDeleteName + ' ?';
+        this.isDialogVisible = true;
+    }
+
+    handleDialogResponse(event){
+        if(event.detail.status == true){
+            this.loaded = false;
+            deleteTile({opportunityId:this.opportunityToDeleteId}).then(data =>{
+                this.loaded = true;
+    
+                this.getTilesData();
+                
+                const toastErrorMessage = new ShowToastEvent({
+                    title: 'Successo',
+                    message: 'Opportunity cancellata con successo',
+                    variant: 'success'
+                });
+                this.dispatchEvent(toastErrorMessage);
+    
+            }).catch(error => {
+                this.loaded = true;
+                console.log(error.message);
+                const toastErrorMessage = new ShowToastEvent({
+                    title: 'Errore',
+                    message: error.message,
+                    variant: 'error'
+                });
+                this.dispatchEvent(toastErrorMessage);
+            });
+        } else {
+            this.isDialogVisible = false;
+        }
     }
 
 }
