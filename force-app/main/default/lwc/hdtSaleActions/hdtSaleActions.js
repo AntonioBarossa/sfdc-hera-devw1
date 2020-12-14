c8d515f144faa045ec3278582e1bb593b15d6121
Keltin Mesonjesi <59123033+keltinMesonjesi@users.noreply.github.com>
2020-12-14 17:51:29 +0100

    Save Sale init

--- force-app/main/default/lwc/hdtSaleActions/hdtSaleActions.js
+++ force-app/main/default/lwc/hdtSaleActions/hdtSaleActions.js
@@ -1,8 +1,11 @@
 import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import saveDraft from '@salesforce/apex/HDT_LC_SaleActions.saveDraft';
+import save from '@salesforce/apex/HDT_LC_SaleActions.save';
 
 export default class hdtSaleActions extends LightningElement {
     @api saleRecord;
+    loading = false;
     currentStep = 4;
 
     get disabledSave(){
@@ -14,6 +17,44 @@ export default class hdtSaleActions extends LightningElement {
         }
 
         return result;
+    }
+
+    handleSaveDraft(){
+        this.loading = true;
+        saveDraft({sale: {Id:this.saleRecord.Id, CurrentStep__c:this.saleRecord.CurrentStep__c}}).then(data =>{
+            this.loading = false;
+            this.dispatchEvent(new CustomEvent('saleupdate', { bubbles: true }));
+        }).catch(error => {
+            this.loading = false;
+            console.log(error.body.message);
+            const toastErrorMessage = new ShowToastEvent({
+                title: 'Errore',
+                message: error.body.message,
+                variant: 'error'
+            });
+            this.dispatchEvent(toastErrorMessage);
+        });
+    }
+
+    handleSave(){
+        
+        console.log('saleRecord: ', JSON.parse(JSON.stringify(this.saleRecord)));
+
+        this.loading = true;
+        save({sale: this.saleRecord}).then(data =>{
+            this.loading = false;
+            this.dispatchEvent(new CustomEvent('saleupdate', { bubbles: true }));
+        }).catch(error => {
+            this.loading = false;
+            console.log(error.body.message);
+            const toastErrorMessage = new ShowToastEvent({
+                title: 'Errore',
+                message: error.body.message,
+                variant: 'error'
+            });
+            this.dispatchEvent(toastErrorMessage);
+        });
+
     }
 
 }
