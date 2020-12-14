7a197311d4daa5b4f788fe0e8601f06bb20da63f
Keltin Mesonjesi <59123033+keltinMesonjesi@users.noreply.github.com>
2020-12-14 10:54:29 +0100

    Steps fixes

--- force-app/main/default/lwc/hdtConfigurePaymentMethods/hdtConfigurePaymentMethods.js
+++ force-app/main/default/lwc/hdtConfigurePaymentMethods/hdtConfigurePaymentMethods.js
@@ -1,36 +1,11 @@
 import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import updateSale from '@salesforce/apex/HDT_LC_ConfigurePaymentMethods.updateSale';
-
 export default class hdtConfigurePaymentMethods extends LightningElement {
     @api saleRecord;
     @api accountId;
     selectedBillingProfile;
     loading = false;
     currentStep = 4;
-    nextStep = 5;
-
-    get hiddenEdit(){
-        let result = true;
-        if(this.saleRecord.CurrentStep__c <= this.currentStep){
-            result = true;
-        } else if(this.saleRecord.CurrentStep__c > this.currentStep){
-            result = false;
-        }
-
-        return result;
-    }
-
-    get disabledNext(){
-        let result = false;
-        if(this.saleRecord.CurrentStep__c != this.currentStep){
-            result = true;
-        } else {
-            result = false;
-        }
-
-        return result;
-    }
 
     get disabledInput(){
         let result = false;
@@ -52,28 +27,4 @@ export default class hdtConfigurePaymentMethods extends LightningElement {
         this.selectedBillingProfile = event.detail;
     }
 
-    updateSaleRecord(saleData){
-        this.loading = true;
-        updateSale({sale: saleData}).then(data =>{
-            this.loading = false;
-            this.dispatchEvent(new CustomEvent('saleupdate', { bubbles: true }));
-        }).catch(error => {
-            this.loading = false;
-            console.log(error.body.message);
-            const toastErrorMessage = new ShowToastEvent({
-                title: 'Errore',
-                message: error.body.message,
-                variant: 'error'
-            });
-            this.dispatchEvent(toastErrorMessage);
-        });
-    }
-
-    handleNext(){
-        this.updateSaleRecord({Id: this.saleRecord.Id, CurrentStep__c: this.nextStep});
-    }
-
-    handleEdit(){
-        this.updateSaleRecord({Id: this.saleRecord.Id, CurrentStep__c: this.currentStep});
-    }
 }
--- force-app/main/default/lwc/hdtSaleActions/hdtSaleActions.js
+++ force-app/main/default/lwc/hdtSaleActions/hdtSaleActions.js
@@ -1,5 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class hdtSaleActions extends LightningElement {
-    
+    @api saleRecord;
+    currentStep = 4;
+
+    get disabledSave(){
+        let result = false;
+        if(this.saleRecord.CurrentStep__c != this.currentStep){
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+
 }
