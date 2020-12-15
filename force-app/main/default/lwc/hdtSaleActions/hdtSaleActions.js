a9e2bcc0cc2d6ce9a55d039f4e2bce1c788ef146
Keltin Mesonjesi <59123033+keltinMesonjesi@users.noreply.github.com>
2020-12-15 18:00:37 +0100

    Cancel Sale

--- force-app/main/default/lwc/hdtSaleActions/hdtSaleActions.js
+++ force-app/main/default/lwc/hdtSaleActions/hdtSaleActions.js
@@ -2,11 +2,16 @@ import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import saveDraft from '@salesforce/apex/HDT_LC_SaleActions.saveDraft';
 import save from '@salesforce/apex/HDT_LC_SaleActions.save';
+import cancel from '@salesforce/apex/HDT_LC_SaleActions.cancel';
 
 export default class hdtSaleActions extends LightningElement {
     @api saleRecord;
     loading = false;
     currentStep = 4;
+    isDialogVisible = false;
+    dialogTitle;
+    dialogMessage;
+    eventType;
 
     get disabledSave(){
         let result = false;
@@ -17,6 +22,52 @@ export default class hdtSaleActions extends LightningElement {
         }
 
         return result;
+    }
+
+    saveSaleCall(){
+        this.loading = true;
+        save({sale: this.saleRecord}).then(data =>{
+            this.loading = false;
+            const toastSuccessMessage = new ShowToastEvent({
+                title: 'Successo',
+                message: 'Vendita salvata con successo',
+                variant: 'success'
+            });
+            this.dispatchEvent(toastSuccessMessage);
+            this.dispatchEvent(new CustomEvent('saleupdate', { bubbles: true }));
+        }).catch(error => {
+            this.loading = false;
+            console.log(error.body.message);
+            const toastErrorMessage = new ShowToastEvent({
+                title: 'Errore',
+                message: error.body.message,
+                variant: 'error'
+            });
+            this.dispatchEvent(toastErrorMessage);
+        });
+    }
+
+    cancelSaleCall(){
+        this.loading = true;
+        cancel({sale: this.saleRecord}).then(data =>{
+            this.loading = false;
+            const toastSuccessMessage = new ShowToastEvent({
+                title: 'Successo',
+                message: 'Vendita anullata con successo',
+                variant: 'success'
+            });
+            this.dispatchEvent(toastSuccessMessage);
+            this.dispatchEvent(new CustomEvent('saleupdate', { bubbles: true }));
+        }).catch(error => {
+            this.loading = false;
+            console.log(error.body.message);
+            const toastErrorMessage = new ShowToastEvent({
+                title: 'Errore',
+                message: error.body.message,
+                variant: 'error'
+            });
+            this.dispatchEvent(toastErrorMessage);
+        });
     }
 
     handleSaveDraft(){
@@ -37,24 +88,35 @@ export default class hdtSaleActions extends LightningElement {
     }
 
     handleSave(){
-        
-        console.log('saleRecord: ', JSON.parse(JSON.stringify(this.saleRecord)));
+        this.eventType = 'saveSale';
+        this.dialogTitle = 'Salva ' + this.saleRecord.Name;
+        this.dialogMessage = 'Vuoi salvare ' + this.saleRecord.Name + ' ?';
+        this.isDialogVisible = true;
+    }
 
-        this.loading = true;
-        save({sale: this.saleRecord}).then(data =>{
-            this.loading = false;
-            this.dispatchEvent(new CustomEvent('saleupdate', { bubbles: true }));
-        }).catch(error => {
-            this.loading = false;
-            console.log(error.body.message);
-            const toastErrorMessage = new ShowToastEvent({
-                title: 'Errore',
-                message: error.body.message,
-                variant: 'error'
-            });
-            this.dispatchEvent(toastErrorMessage);
-        });
+    handleCancel(){
+        this.eventType = 'cancelSale';
+        this.dialogTitle = 'Anulla ' + this.saleRecord.Name;
+        this.dialogMessage = 'Vuoi anullare ' + this.saleRecord.Name + ' ?';
+        this.isDialogVisible = true;
+    }
 
+    handleDialogResponse(event){
+        if(event.detail.status == true){
+            console.log(this.eventType);
+            switch (this.eventType) {
+                case 'saveSale':
+                    this.saveSaleCall();
+                    break;
+                case 'cancelSale':
+                    this.cancelSaleCall();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            this.isDialogVisible = false;
+        }
     }
 
 }
