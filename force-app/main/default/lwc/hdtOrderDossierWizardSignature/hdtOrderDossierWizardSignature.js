7b8626f9d93533bd4675f799c2caad6ef03ad666
Keltin Mesonjesi <59123033+keltinMesonjesi@users.noreply.github.com>
2021-01-19 10:35:36 +0100

    Order dossier signature fixes

--- force-app/main/default/lwc/hdtOrderDossierWizardSignature/hdtOrderDossierWizardSignature.js
+++ force-app/main/default/lwc/hdtOrderDossierWizardSignature/hdtOrderDossierWizardSignature.js
@@ -11,10 +11,17 @@ export default class hdtOrderDossierWizardSignature extends LightningElement {
     disabledInput = false;
     loading = false;
     isMailVisible = false;
+    isAddrVisible = false;
     primaryContactEmail = '';
-    ordChildList = [];
+    accountAddr = '';
+    ordChildBpAddr = '';
+    addressOptions = [];
     get mailClasses(){
         return this.isMailVisible ? 'slds-size_1-of-2 slds-show' : 'slds-size_1-of-2 slds-hide';
+    }
+
+    get addrClasses(){
+        return this.isAddrVisible ? 'slds-size_1-of-2 slds-show' : 'slds-size_1-of-2 slds-hide';
     }
 
     get hiddenEdit(){
@@ -50,6 +57,19 @@ export default class hdtOrderDossierWizardSignature extends LightningElement {
         // return result;
     // }
 
+    outputFormatedAddress(address){
+
+        let streetName = (address.streetName !== undefined && address.streetName !== '')  ? (address.streetName + ' ') : 'ok';
+        let streetNumber = (address.streetNumber !== undefined && address.streetNumber !== '') ? (address.streetNumber + ' ') : '';
+        let streetNumberExtension = (address.streetNumberExtension !== undefined && address.streetNumberExtension !== '') ? (address.streetNumberExtension + ', ') : '';
+        let place = (address.place !== undefined && address.place !== '') ? (address.place + ' ') : '';
+        let province = (address.province !== undefined && address.province !== '') ? (address.province + ', ') : '';
+        let postalCode = (address.postalCode !== undefined && address.postalCode !== '') ? (address.postalCode + ' ') : '';
+        let country = (address.country !== undefined && address.country !== '') ? address.country : '';
+
+        return streetName + streetNumber + streetNumberExtension + place + province + postalCode + country;
+    }
+
     handleDataCollection(event){
         let fieldName = event.target.fieldName;
         let fieldValue = event.target.value;
@@ -63,11 +83,17 @@ export default class hdtOrderDossierWizardSignature extends LightningElement {
 
         if (fieldName === 'DocSendingMethod__c') {
             this.isMailVisible = (fieldValue === 'Mail');
+            this.isAddrVisible = (fieldValue === 'Posta');
         }
 
-        if (fieldName === 'SignatureMethod__c' && (fieldValue === 'OTP' || fieldValue === 'Vocal Order')){
+        if (fieldName === 'SignatureMethod__c'){
             this.isMailVisible = (this.template.querySelector("[data-id='DocSendingMethod__c']").value === 'Mail');
+            this.isAddrVisible = (this.template.querySelector("[data-id='DocSendingMethod__c']").value === 'Posta');
         }
+
+        // if (fieldName === 'SignatureMethod__c' && (fieldValue === 'OTP' || fieldValue === 'Stampa')){
+        //     this.isAddrVisible = (this.template.querySelector("[data-id='DocSendingMethod__c']").value === 'Posta');
+        // }
 
     }
 
@@ -105,8 +131,22 @@ export default class hdtOrderDossierWizardSignature extends LightningElement {
         controllerInit({orderParentId: this.orderParentRecord.Id, accountId: this.orderParentRecord.AccountId}).then(data =>{
             this.loading = false;
             this.primaryContactEmail = data.primaryContactEmail;
-            console.log('data.ordChildList: ', data.ordChildList);
-            this.ordChildList = data.ordChildList;
+            this.ordChildBpAddr = data.ordChildList[0].BillingProfile__r;
+
+            this.ordChildBpAddr = this.outputFormatedAddress({
+                streetName: data.ordChildList[0].BillingProfile__r.InvoicingStreetName__c,
+                streetNumber: data.ordChildList[0].BillingProfile__r.InvoicingStreetNumber__c,
+                streetNumberExtension: data.ordChildList[0].BillingProfile__r.InvoicingStreetNumberExtension__c,
+                place: data.ordChildList[0].BillingProfile__r.InvoicingPlace__c,
+                province: data.ordChildList[0].BillingProfile__r.InvoicingProvince__c,
+                postalCode: data.ordChildList[0].BillingProfile__r.InvoicingPostalCode__c,
+                country: data.ordChildList[0].BillingProfile__r.InvoicingCountry__c
+            });
+
+            if (this.ordChildBpAddr !== '') {
+                this.addressOptions.push({label: this.ordChildBpAddr, value: this.ordChildBpAddr});
+            }
+
         }).catch(error => {
             this.loading = false;
             console.log(error.body.message);
@@ -120,7 +160,18 @@ export default class hdtOrderDossierWizardSignature extends LightningElement {
     }
 
     connectedCallback(){
-        console.log('Account address: ', JSON.stringify(this.orderParentRecord.Account.BillingAddress));
+
+        this.accountAddr = this.outputFormatedAddress({
+            streetName: this.orderParentRecord.Account.BillingAddress.street,
+            place: this.orderParentRecord.Account.BillingAddress.city,
+            postalCode: this.orderParentRecord.Account.BillingAddress.postalCode,
+            country: this.orderParentRecord.Account.BillingAddress.country
+        });
+
+        if (this.accountAddr !== '') {
+            this.addressOptions.push({'label': this.accountAddr, 'value': this.accountAddr});
+        }
+
         this.handleFormInit();
         this.handleControllerInit();
     }
