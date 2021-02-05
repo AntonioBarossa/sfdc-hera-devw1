8075bcadfd84037e039e296b812ffdb7a8669156
Francesco Panico <frpanico@everis.com>
2021-02-05 11:16:31 +0100

    [HRADTR-362] Sviluppi Preventivi 05/02 11.16

---
+++ force-app/main/default/lwc/hdtDocumentSignatureManagerFlow/hdtDocumentSignatureManagerFlow.js
@@ -0,0 +1,25 @@
+import { LightningElement,track,api } from 'lwc';
+import { FlowAttributeChangeEvent, FlowNavigationNextEvent, FlowNavigationFinishEvent,FlowNavigationBackEvent  } from 'lightning/flowSupport';
+
+export default class HdtDocumentSignatureManagerFlow extends LightningElement {
+    @api processType;
+    @api quoteType;
+    @api outcome;
+    @api availableActions = [];
+
+    handleGoNext() {
+        if(this.availableActions.find(action => action === 'NEXT')){
+
+            const navigateNextEvent = new FlowNavigationNextEvent();
+
+            this.dispatchEvent(navigateNextEvent);
+
+        } else {
+
+            const navigateFinish = new FlowNavigationFinishEvent();
+
+            this.dispatchEvent(navigateFinish);
+        }
+
+    }
+}
