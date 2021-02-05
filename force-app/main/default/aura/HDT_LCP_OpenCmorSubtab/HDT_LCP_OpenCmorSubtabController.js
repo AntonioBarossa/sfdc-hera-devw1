9bf1ca3178cba0c254bb253813e6ead36c03afe7
dmontors <daniele.montorselli@everis.com>
2021-02-05 14:34:33 +0100

    [HRADTR-14] open lwc

--- force-app/main/default/aura/HDT_LCP_OpenCmorSubtab/HDT_LCP_OpenCmorSubtabController.js
+++ force-app/main/default/aura/HDT_LCP_OpenCmorSubtab/HDT_LCP_OpenCmorSubtabController.js
@@ -1,38 +1,5 @@
 ({
-    openTabWithSubtab : function(component, event, helper) {
-
-        var accountId = component.get("v.recordId");
-        var workspaceAPI = component.find("workspace");
-
-        workspaceAPI.openTab({
-            url: '/' + accountId
-        }).then(function(response) {
-            var i = workspaceAPI.openSubtab({
-                parentTabId: response,
-                pageReference: {
-                    type: 'standard__component',
-                    attributes: {
-                        componentName: 'c__HDT_LCP_ViewLwcSubTab'
-                    },
-                    state: {
-                        c__recordid: accountId,
-                        c__type: 'cmor'
-                    }
-				}
-            });
-
-            workspaceAPI.setTabLabel({
-                tabId: i,
-                label: 'CMOR'
-            });
-            workspaceAPI.setTabIcon({
-                tabId: i,
-                icon: 'custom:custom83'
-            });
-            $A.get("e.force:closeQuickAction").fire();
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+    openTabWithSubtab: function(component, event, helper) {
+        helper.openTabWithSubtabHelp(component, event, helper);
     }
 })
--- force-app/main/default/aura/HDT_LCP_OpenCmorSubtab/HDT_LCP_OpenCmorSubtabHelper.js
+++ force-app/main/default/aura/HDT_LCP_OpenCmorSubtab/HDT_LCP_OpenCmorSubtabHelper.js
@@ -1,5 +1,40 @@
 ({
-    helperMethod : function() {
+    openTabWithSubtabHelp : function(component, event, helper) {
 
+        var accountId = component.get("v.recordId");
+        var workspaceAPI = component.find("workspace");
+
+        $A.get("e.force:closeQuickAction").fire();
+
+        workspaceAPI.openTab({
+            url: '/' + accountId
+        }).then(function(response) {
+            var i = workspaceAPI.openSubtab({
+                parentTabId: response,
+                pageReference: {
+                    type: 'standard__component',
+                    attributes: {
+                        componentName: 'c__HDT_LCP_ViewLwcSubTab'
+                    },
+                    state: {
+                        c__recordid: accountId,
+                        c__type: 'cmor'
+                    }
+				}
+            });
+
+            workspaceAPI.setTabLabel({
+                tabId: i,
+                label: 'CMOR'
+            });
+            workspaceAPI.setTabIcon({
+                tabId: i,
+                icon: 'custom:custom83'
+            });
+            
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
     }
 })
