219b7895607b99a21a1685f019c2727bfe5b715d
dmontors <daniele.montorselli@everis.com>
2021-02-23 17:33:52 +0100

    [HRADTR-325] search criteria input

--- force-app/main/default/lwc/hdtEligibilityCriteriaConfiguration/hdtEligibilityCriteriaConfiguration.js
+++ force-app/main/default/lwc/hdtEligibilityCriteriaConfiguration/hdtEligibilityCriteriaConfiguration.js
@@ -10,11 +10,18 @@ export default class HdtEligibilityCriteriaConfiguration extends LightningElemen
     @track dataToView = [];
     @track dataRemoved = [];
     showTable = true;
+    buttonLabel = 'uguale a';
+    operator = 'equal';
+    showEmptyImmage = true;
     showAvailableItems = false;
+    showSearchTable = false;
     queryTerm;
     provinceOptions;
     currentProvinceId;
-    @track searchList = [];
+    disabled = '';
+
+    @track searchTable = [];
+    //enableSearch = false;
 
     @track spinnerObj = {
         spinner: false,
@@ -80,10 +87,71 @@ export default class HdtEligibilityCriteriaConfiguration extends LightningElemen
         //this.storeData = foundProvince.cityAvailable;//+++
         this.dataToView = foundProvince.cityAvailable;
         this.dataRemoved = foundProvince.cityRemoved;
+        this.showEmptyImmage = false;
         this.showAvailableItems = true;
+        this.showSearchTable = false;
+    }
+    
+    changeOperator(event){
+        if(event.target.name === 'equal'){
+            this.buttonLabel = 'diverso da';
+            event.target.name = 'notequal';
+            this.operator = 'notequal';
+        } else {
+            this.buttonLabel = 'uguale a';
+            event.target.name = 'equal';
+            this.operator = 'equal';
+        }
     }
 
     handleOnchange(event) {
+        console.log('# handleOnchange #');
+        console.log('# this.operator > ' + this.operator);
+
+        this.queryTerm = event.target.value;
+        console.log('# search -> ' + this.queryTerm);
+
+        this.disabled = 'dis';
+        this.showEmptyImmage = false;
+        this.showAvailableItems = false;
+        this.showSearchTable = true;
+
+        if(this.queryTerm != null && this.queryTerm != '' && this.queryTerm != undefined){
+            
+            var lowerTerm = this.queryTerm.toLowerCase();
+            console.log('# find: ' + lowerTerm );
+
+            this.searchTable = [];
+            for(var i=0; i<this.dataToView.length; i++){
+                var currentLabel = this.dataToView[i].label.toLowerCase();
+                var cap = this.dataToView[i].value;
+
+                if(this.operator === 'equal'){
+                    if(currentLabel.startsWith(lowerTerm) || cap.startsWith(this.queryTerm)){
+                        this.searchTable.push(this.dataToView[i]);
+                    }
+                } else {
+                    if(!currentLabel.includes(lowerTerm)){
+                        this.searchTable.push(this.dataToView[i]);
+                    }                    
+                }
+
+            }
+
+        } else {
+            this.searchTable = [];
+            this.disabled = '';
+            this.showEmptyImmage = false;
+            this.showAvailableItems = true;
+            this.showSearchTable = false;
+
+            //for(var i=0; i<this.dataToView.length; i++){
+            //    this.searchTable.push(this.dataToView[i]);
+            //}
+        }
+    }
+
+    /*handleOnchange(event) {
         console.log('# handleOnchange #');
 
         this.queryTerm = event.target.value;
@@ -110,7 +178,7 @@ export default class HdtEligibilityCriteriaConfiguration extends LightningElemen
                 this.dataToView.push(this.storeData[i]);
             }
         }
-    }
+    }*/
 
     searchField(event) {
         console.log('# searchField #');
@@ -118,6 +186,23 @@ export default class HdtEligibilityCriteriaConfiguration extends LightningElemen
 
     searchCity(event){
         console.log('# searchCity #');
+    }
+
+    removeAllItems(event){
+        console.log('# removeAllItems #');
+
+        this.searchTable.forEach((i) => {
+            //find and remove item from filtered list
+            let element = this.dataToView.find(ele  => ele.value === i.value);
+            var index = this.dataToView.indexOf(element);
+            this.dataToView.splice(index, 1);
+
+            var itemRemoved = { label: i.label, value: i.value, id: i.id};
+            this.dataRemoved.push(itemRemoved);
+        });
+
+        this.showAvailableItems = true;
+        this.showSearchTable = false;
     }
 
     removeItem(event){
@@ -128,6 +213,15 @@ export default class HdtEligibilityCriteriaConfiguration extends LightningElemen
         var itemValue = event.currentTarget.getAttribute('data-value');
         console.log('# Remove ' + itemId + '; ' + itemLabel + '; ' + itemValue);
 
+        var tableType = event.target.name;
+        console.log('# tableType > ' + tableType);
+
+        if(tableType != null && tableType != '' && tableType != undefined && tableType === 'searchTable'){
+            let element = this.searchTable.find(ele  => ele.value === itemId);
+            var index = this.searchTable.indexOf(element);
+            this.searchTable.splice(index, 1);           
+        }
+
         //find and remove item from filtered list
         let element = this.dataToView.find(ele  => ele.value === itemId);
         var index = this.dataToView.indexOf(element);
@@ -136,9 +230,9 @@ export default class HdtEligibilityCriteriaConfiguration extends LightningElemen
         var itemRemoved = { label: itemLabel, value: itemValue, id: itemId};
         this.dataRemoved.push(itemRemoved);
 
-        if(!this.showTable){
-            this.showTable = true;
-        }
+        //if(!this.showTable){
+        //    this.showTable = true;
+        //}
     }
 
     restoreItem(event){
