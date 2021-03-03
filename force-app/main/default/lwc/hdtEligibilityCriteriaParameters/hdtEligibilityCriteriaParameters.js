f93afe2c8388e80bcd920f744af8ed6adbf1615f
dmontors <daniele.montorselli@everis.com>
2021-03-03 09:15:13 +0100

    [HRADTR-325] Eligibility criteria param

--- force-app/main/default/lwc/hdtEligibilityCriteriaParameters/hdtEligibilityCriteriaParameters.js
+++ force-app/main/default/lwc/hdtEligibilityCriteriaParameters/hdtEligibilityCriteriaParameters.js
@@ -7,7 +7,6 @@ export default class HdtEligibilityCriteriaParameters extends LightningElement {
        {row: '2', innerList: ['ATC Gas', 'Nuovo Cliente']},
        {row: '3', innerList: ['Campagna', 'Opzione Energia Verde']},
        {row: '4', innerList: ['Canale', 'Opzione Gas Verde']},
-       {row: '5', innerList: ['Categoria Uso', 'Prodotto']},
        {row: '6', innerList: ['Codice Criterio', 'Provenienza']},
        {row: '7', innerList: ['Company Owner', 'Raggruppamento di Login']},
        {row: '8', innerList: ['Eta Cliente', 'Ruolo Profilo']},
