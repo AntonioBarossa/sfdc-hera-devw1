1442a41d5b96ed6bcc2411d6461ffbc545359e2b
Lorenzo Rollo <lorenzo.rollo@everis.com>
2021-03-25 16:47:34 +0100

    HRADTR-435 Genesys integration progress

--- force-app/main/default/staticresources/iwsscript.js
+++ force-app/main/default/staticresources/iwsscript.js
@@ -1,3 +1,28 @@
+/********** CUSTOM **********/
+
+function onEventEstablishedInbound(message) {
+    sfutil.createActivity(message);
+}
+function onEventMarkDoneInbound(message) {
+    sfutil.closeActivity(message, false, false);
+}
+function onEventEstablishedOutbound(message) {
+    sfutil.createActivity(message);
+}
+function onEventMarkDoneOutbound(message) {
+    sfutil.closeActivity(message, false, false);
+}
+function onEventEstablishedInternal(message) {
+    sfutil.createActivity(message);
+}
+function onEventMarkDoneInternal(message) {
+    sfutil.closeActivity(message, false, true);
+}
+function onSwitchCrmInbound(message) {
+    sfutil.closeActivity(message, true, false);
+}
+
+/********* STANDARD *********/
 var softphone_connector_initialized = false;
 function networkError(message) {
     log.error(message);
@@ -67,19 +92,11 @@ function onEventRingingConsult(message) {
 }
 function onEventRingingOutbound(message) {
 }
-function onEventEstablishedInbound(message) {
-    logsf.info("onEventEstablishedInbound , message : ", message);
-    sfutil.createTask(message, 'Phone', message.ANI.replace("tel:", ""), message.MediaType + " - " + message.ConnectionID || message.callId);
-}
 function onEventPartyChangedInbound(message) {
 }
 function onEventPartyChangedOutbound(message) {
 }
-function onEventEstablishedInternal(message) {
-}
 function onEventEstablishedConsult(message) {
-}
-function onEventEstablishedOutbound(message) {
 }
 function onEventHeldInbound(message) {
 }
@@ -226,6 +243,4 @@ function onWorkitemEventMarkDoneInbound(message) {
 }
 function onWorkitemEventRingingInbound(message) {
     log.debug("Called onWorkitemEventRingingInbound: ");
-}
-function onEventMarkDoneOutbound(message) {
 }
