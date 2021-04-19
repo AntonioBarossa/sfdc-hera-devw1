416bb29053c2dfca7ef6f7a428fedcd06255aaa2
dmontors <daniele.montorselli@everis.com>
2021-04-19 19:47:47 +0200

    [HRADTR-325] EligibilityCriteria fields update

--- force-app/main/default/lwc/hdtEligibilityCriteriaParameters/hdtEligibilityCriteriaParameters.js
+++ force-app/main/default/lwc/hdtEligibilityCriteriaParameters/hdtEligibilityCriteriaParameters.js
@@ -3,48 +3,21 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class HdtEligibilityCriteriaParameters extends LightningElement {
 
-    /*@track fields = [
-       {row: '1', innerList: ['Agenzia', 'Marcatura Cliente']},
-       {row: '2', innerList: ['ATC Gas', 'Nuovo Cliente']},
-       {row: '3', innerList: ['Campagna', 'Opzione Energia Verde']},
-       {row: '4', innerList: ['Canale', 'Opzione Gas Verde']},
-       {row: '6', innerList: ['Codice Criterio', 'Provenienza']},
-       {row: '7', innerList: ['Company Owner', 'Raggruppamento di Login']},
-       {row: '8', innerList: ['Eta Cliente', 'Ruolo Profilo']},
-       {row: '9', innerList: ['Fascia', 'Tipo Apparecchiatura']},
-       {row: '10', innerList: ['Login', 'Tipo Cliente Categoria']}
-    ];*/
+      @track fieldsList = [
+        'Channel__c',            'OriginMarket__c',
+        'ClientMarker__c',       'CategoryTypeClient__c',
+        'Login__c',              'LoginGroup__c',
+        'NewClient__c',          'ResidentDeliveryAddress__c',
+        'Agency__c',             'CompanyOwner__c',
+        'UseCategory__c',        'Campaign__c',
+        'EquipmenType__c',       'PromoCode__c'
+      ];
 
-    @track fieldsList = [
-        {
-          id: '1',
-          list: [
-            'Channel__c',
-            'Agency__c',
-            'Login__c',
-            'LoginGroup__c',
-            'ClientMarker__c',
-            'CategoryTypeClient__c',
-            'NewClient__c',
-            'CompanyOwner__c',
-            'ClientAge__c',
-            'Campaign__c'
-          ]
-        },
-        {
-          id: '2',
-          list: [
-            'EquipmenType__c',
-            'UseCategory__c',
-            'ConsumptionRangeEE__c',
-            'ConsumptionRangeGAS__c',
-            'PowerRange__c',
-            'OriginMarket__c',
-            'ResidentDeliveryAddress__c',
-            'PaymentMode__c',
-            'BillSendingMethod__c'
-          ]
-        }
+      @track complexFieldsList = [
+        {fieldName: 'ClientAge', fields: ['ClientAgeMin__c', 'ClientAgeMax__c']},
+        {fieldName: 'ConsumptionRangeEE', fields: ['ConsumptionRangeEEmin__c', 'ConsumptionRangeEEmax__c']},
+        {fieldName: 'ConsumptionRangeGAS', fields: ['ConsumptionRangeGASmin__c', 'ConsumptionRangeGASmax__c']},
+        {fieldName: 'PowerRange', fields: ['PowerRangeMin__c', 'PowerRangeMax__c']}
       ];
 
     @api productid;
@@ -96,5 +69,49 @@ export default class HdtEligibilityCriteriaParameters extends LightningElement {
       this.dispatchEvent(saverecord);
       //this.template.querySelector('lightning-record-edit-form').submit();
     }
+
+    /*@track fields = [
+       {row: '1', innerList: ['Agenzia', 'Marcatura Cliente']},
+       {row: '2', innerList: ['ATC Gas', 'Nuovo Cliente']},
+       {row: '3', innerList: ['Campagna', 'Opzione Energia Verde']},
+       {row: '4', innerList: ['Canale', 'Opzione Gas Verde']},
+       {row: '6', innerList: ['Codice Criterio', 'Provenienza']},
+       {row: '7', innerList: ['Company Owner', 'Raggruppamento di Login']},
+       {row: '8', innerList: ['Eta Cliente', 'Ruolo Profilo']},
+       {row: '9', innerList: ['Fascia', 'Tipo Apparecchiatura']},
+       {row: '10', innerList: ['Login', 'Tipo Cliente Categoria']}
+    ];*/
+
+    /*@track fieldsList = [
+        {
+          id: '1',
+          list: [
+            'Channel__c',
+            'Agency__c',
+            'Login__c',
+            'LoginGroup__c',
+            'ClientMarker__c',
+            'CategoryTypeClient__c',
+            'NewClient__c',
+            'CompanyOwner__c',
+            'ClientAge__c',
+            'Campaign__c'
+          ]
+        },
+        {
+          id: '2',
+          list: [
+            'EquipmenType__c',
+            'UseCategory__c',
+            'ConsumptionRangeEE__c',
+            'ConsumptionRangeGAS__c',
+            'PowerRange__c',
+            'OriginMarket__c',
+            'ResidentDeliveryAddress__c'//,
+            ///'PaymentMode__c',
+            ///'BillSendingMethod__c'
+          ]
+        }
+      ];*/
 
 }
