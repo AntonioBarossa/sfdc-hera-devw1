ff11d9ab85a426bb5e034c045a393224021fcb5b
fatjola <grp_hera_wrdev3@eng.it>
2021-04-20 14:56:39 +0200

    orderDossier updated

--- force-app/main/default/lwc/hdtOrderDossierWizardHeader/hdtOrderDossierWizardHeader.js
+++ force-app/main/default/lwc/hdtOrderDossierWizardHeader/hdtOrderDossierWizardHeader.js
@@ -15,17 +15,19 @@ export default class hdtOrderDossierWizardHeader extends LightningElement {
     isAccountBusiness = false;
 
     connectedCallback(){
-        console.log('orderParentRecord: ',JSON.parse(JSON.stringify(this.orderParentRecord)));
+        console.log(' hdtOrderDossierWizardHeader - orderParentRecord: ',JSON.parse(JSON.stringify(this.orderParentRecord)));
 
         this.orderName = this.orderParentRecord.Name;
         this.orderNumber = this.orderParentRecord.OrderNumber;
         this.accountName = this.orderParentRecord.Account.Name;
-        this.firstName = this.orderParentRecord.Account.FirstName__c;
-        this.lastName = this.orderParentRecord.Account.LastName__c;
-        this.accountFiscalCode = this.orderParentRecord.Account.FiscalCode__c;
-        this.accountVatNumber = this.orderParentRecord.Account.VATNumber__c;
-        this.phase = this.orderParentRecord.Phase__c;
+this.firstName = this.orderParentRecord.Account.FirstName__c !== undefined ? this.orderParentRecord.Account.FirstName__c : '';
+        this.lastName = this.orderParentRecord.Account.LastName__c !== undefined ? this.orderParentRecord.Account.LastName__c : '';
+        this.accountFiscalCode = this.orderParentRecord.Account.FiscalCode__c !== undefined ? this.orderParentRecord.Account.FiscalCode__c : '';
+        this.accountVatNumber = this.orderParentRecord.Account.VATNumber__c !== undefined ? this.orderParentRecord.Account.VATNumber__c : '';
+        this.phase = this.orderParentRecord.Phase__c !== undefined ? this.orderParentRecord.Phase__c : '';
         this.createdDate = this.orderParentRecord.CreatedDate;
-        this.isAccountBusiness = this.orderParentRecord.Account.RecordType.DeveloperName === 'HDT_RT_Business';
+        if(this.orderParentRecord.Account.RecordType !== undefined){
+            this.isAccountBusiness = this.orderParentRecord.Account.RecordType.DeveloperName === 'HDT_RT_Business';
+        }
     }
 }
