13d59b2419199c0fc2f80193231d8fb47a621470
Elvis Angelaccio <elvis.angelaccio@everis.com>
2021-04-21 23:25:52 +0200

    [HRADTR-15] Fix filter

--- force-app/main/default/lwc/hdtMeterReading/hdtMeterReading.js
+++ force-app/main/default/lwc/hdtMeterReading/hdtMeterReading.js
@@ -76,7 +76,7 @@ export default class HdtMeterReading extends LightningElement {
         this.contractDataToView = [];
 
         if(event.target.value!=''){
-            var filteredContract = this.contractData.filter(c => { return c.contractNumber == event.target.value });
+            var filteredContract = this.contractData.filter(c => { return c.contractNumber.includes(event.target.value) });
 
             if(filteredContract.length>0){
                 this.contractDataToView = filteredContract;
