5cc7390eaf331b0ffd25b18a60b8422bcca7cc17
lorenzo <lorenzo.gulotta@webresults.it>
2021-06-04 18:33:50 +0200

    [fix]

--- force-app/main/default/lwc/hdtFormAccountResidenziale/hdtFormAccountResidenziale.js
+++ force-app/main/default/lwc/hdtFormAccountResidenziale/hdtFormAccountResidenziale.js
@@ -298,8 +298,8 @@ export default class HdtFormAccountResidenziale extends NavigationMixin(Lightnin
                 messageError=" Il numero di cellulare deve essere compreso tra le 9 e le 12 cifre!";
             }
         }
-        if(!(fiscalCode.value=== undefined || fiscalCode.value.trim()==='')){
-            if(fiscalCode.value.length == 16){
+        if(!(this.fiscalCode.value=== undefined || this.fiscalCode.value.trim()==='')){
+            if(this.fiscalCode.value.length == 16){
                 isValidated=false;
                 messageError=" Il Codice fiscale deve essere lungo 16 cifre!";
             }
