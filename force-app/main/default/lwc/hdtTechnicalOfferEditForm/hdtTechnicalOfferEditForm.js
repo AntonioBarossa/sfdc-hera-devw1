093785d2038bca2d8f01289260c74d96995dbc7b
dmontors <daniele.montorselli@everis.com>
2021-06-04 17:54:15 +0200

    [HRADTR-325] Product2 batch and rate values

--- force-app/main/default/lwc/hdtTechnicalOfferEditForm/hdtTechnicalOfferEditForm.js
+++ force-app/main/default/lwc/hdtTechnicalOfferEditForm/hdtTechnicalOfferEditForm.js
@@ -1,17 +1,20 @@
 import { LightningElement, api} from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class HdtTechnicalOfferEditForm extends LightningElement {
+
     @api productId;
+    @api rateObj;
     technicalOfferId = '';
     techOffId;
     fieldsList = [
         'Market__c',
         'ProcessType__c',
         'PlacetOffer__c',
-        'ServiceProduct__c',
-        'StepAllowed__c',
+        //'ServiceProduct__c',
         'StartDate__c',
         'EndDate__c',
+        'StepAllowed__c',
         'ContractId__c',
         'NumberTimeExtension__c',
         'UnitTimeExtension__c',
@@ -23,7 +26,23 @@ export default class HdtTechnicalOfferEditForm extends LightningElement {
         'UnitOfTimeMeasurement__c',
         'AdmittingProfileModification__c',
         'OfferToBeModified__c'
-      ];
+    ];
+
+    connectedCallback(){
+        
+        if(this.rateObj.rateName!=null && this.rateObj.rateName != undefined){
+            console.log('>>> rate: ' + this.rateObj.rateName);
+        }
+
+        if(this.rateObj.rateTemplate!=null && this.rateObj.rateTemplate != undefined){
+            console.log('>>> template: ' + this.rateObj.rateTemplate);
+        }
+
+        if(this.rateObj.servProduct!=null && this.rateObj.servProduct != undefined){
+            console.log('>>> servProduct: ' + this.rateObj.servProduct);
+        }
+
+    }
 
     handleLoad(event){
         console.log('>>>> handleLoad ');
@@ -58,6 +77,25 @@ export default class HdtTechnicalOfferEditForm extends LightningElement {
             techOffObj[field.fieldName] = field.value;
         });
 
+        if(this.rateObj.servProduct!=null && this.rateObj.servProduct != undefined){
+            techOffObj.ServiceProduct__c = this.rateObj.servProduct;
+        }
+
+        var respCheck = this.checkValue(techOffObj);
+        console.log('#### ' + respCheck.success);
+
+        if(!respCheck.success){
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Attenzione',
+                    message: respCheck.message,
+                    variant: 'warning'
+                })
+            );
+            return;
+        }
+
+
         const newOffer = new CustomEvent('newoffercreated', {
             detail: {newTechOfferObj: JSON.stringify(techOffObj)}
         });
@@ -72,6 +110,41 @@ export default class HdtTechnicalOfferEditForm extends LightningElement {
         });
         // Fire the custom event
         this.dispatchEvent(closemodal);
+    }
+
+    checkValue(techOffObj){
+        console.log('>>> checkValue ');
+
+        var returnObj = {
+            success: false,
+            message: ''
+        };
+
+        if(this.checkIsNotNull(techOffObj.StartDate__c) && this.checkIsNotNull(techOffObj.EndDate__c)){
+           
+            var start = new Date(techOffObj.StartDate__c);
+            var end = new Date(techOffObj.EndDate__c);
+
+            if(start >= end){
+                returnObj.message = 'Data fine inferiore dello start';
+                return returnObj;
+            }
+
+            returnObj.success = true;
+            return returnObj;
+
+        }
+
+        returnObj.success = true;
+        return returnObj;
+    }
+
+    checkIsNotNull(valueToCheck){
+        if(valueToCheck !== undefined && valueToCheck !== '' && valueToCheck !== null){
+            return true;
+        } else {
+            return false;
+        }        
     }
 
 }
