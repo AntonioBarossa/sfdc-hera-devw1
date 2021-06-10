a707cc68322952907389dbc97f95d03ea1b1fa43
LucaMolino <Luca.Molino@eng.it>
2021-06-10 16:37:40 +0200

    release fix Resident__c e BillingProfile

--- force-app/main/default/lwc/hdtTargetObjectCreateForm/hdtTargetObjectCreateForm.js
+++ force-app/main/default/lwc/hdtTargetObjectCreateForm/hdtTargetObjectCreateForm.js
@@ -66,7 +66,7 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
     isValid = false;
     isValidFields = false;
     @api isricercainsap;
-
+   // showForm=false;
 
     
     
@@ -202,7 +202,7 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
                     }
                 ) 
             }
-                        else if(element=='SAPImplantCode__c')
+            else if(element=='SAPImplantCode__c')
             {
 
                 fieldsDataObject.push(
@@ -288,6 +288,18 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
                             required : mapFieldReq.get(element),
                             value:this.servicePointRetrievedData[element],
                             disabled: true
+                        }
+                    ) 
+                }
+                else if(this.recordtype.label === 'Punto Elettrico' && element === 'Resident__c'){
+                    console.log('entra in resident');
+                    this.allSubmitedFields.Resident__c = true;
+                    fieldsDataObject.push(
+                        {
+                            fieldname: element,
+                            required : mapFieldReq.get(element),
+                            value:true,
+                            disabled: false
                         }
                     ) 
                 }
@@ -492,6 +504,7 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
         this.allFieldsObject = this.toObject(this.allFieldsData, this.allFieldsDataReq);
     }
 
+
     connectedCallback(){
         this.loading = true;
 
@@ -584,6 +597,7 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
             });
             this.dispatchEvent(toastErrorMessage);
         });
+       // this.showForm=true;
     }
 
 
@@ -610,6 +624,7 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
      * Pre-fill Account__c field on render
      */
     renderedCallback(){
+        console.log('renderedCallback START + RECORDTYPEID' + this.recordType);
         if(this.fieldsReady){
             if(this.selectedservicepoint == undefined){
                 let accountField = this.template.querySelector('[data-name="Account__c"]');
@@ -1140,6 +1155,7 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
                 variant: 'error'
             });
             this.dispatchEvent(toastErrorMessage);
+
         });
 									 
     }
