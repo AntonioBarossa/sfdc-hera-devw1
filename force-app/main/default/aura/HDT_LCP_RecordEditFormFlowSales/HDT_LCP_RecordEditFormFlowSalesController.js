ea5e109019a8ee14f346efc334451ab1a9e04b96
lorenzo <lorenzo.gulotta@webresults.it>
2021-06-16 19:40:25 +0200

    [fix]

--- force-app/main/default/aura/HDT_LCP_RecordEditFormFlowSales/HDT_LCP_RecordEditFormFlowSalesController.js
+++ force-app/main/default/aura/HDT_LCP_RecordEditFormFlowSales/HDT_LCP_RecordEditFormFlowSalesController.js
@@ -1,4 +1,13 @@
 ({
+
+    doInit : function(component, event, helper) {
+        var pageReference = component.get("v.pageReference");
+        if(pageReference != null && pageReference.state !== null && pageReference.state.c__caseId !== null){
+           component.set("v.recordId", pageReference.state.c__caseId);
+        }
+        component.set("v.showComp",true);
+    },
+
     closeTabMain : function(component, event, helper) {
         console.log("PROVAILLIGHTNING");
         var workspaceAPI = component.find("workspace");
