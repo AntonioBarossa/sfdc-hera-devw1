d7d1bebf7b79b287370d7ce39e07e80ae1c3035e
fdefelice <federico.defelice@webresults.it>
2021-07-12 11:08:10 +0200

    Proroga connessione Temporanea

--- force-app/main/default/lwc/hdtSelezioneFornituraFlow/hdtSelezioneFornituraFlow.js
+++ force-app/main/default/lwc/hdtSelezioneFornituraFlow/hdtSelezioneFornituraFlow.js
@@ -1,7 +1,6 @@
 import  SaleVas  from 'c/hdtSaleVas';
-//import SaleVasHtml from 'c/hdtSaleVas/hdtSaleVas.html'
-import { api } from 'lwc';
-//import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+import { api, track } from 'lwc';
+import getTempNuovaAttivazContracts from '@salesforce/apex/HDT_LC_SaleVas.getTempNuovaAttivazContracts';
 
 export default class HdtSelezioneFornituraFlow extends SaleVas {
     @api accountId;
@@ -10,9 +9,21 @@ export default class HdtSelezioneFornituraFlow extends SaleVas {
         return this.selectedOrder==undefined? null : this.selectedOrder.Id;
     }
 
-    @api groupOptions;//Ordini in corso;Contratti Attivi;
+    @api get contrId(){
+        return this.selectedContract==undefined? null : this.selectedContract.Id;
+    }
 
-    ordersListcolumns = [
+    @api groupOption;//Ordini in corso;Contratti Attivi;
+
+    get contextObject(){
+        let option = this.groupOption;
+        switch(option){
+            case 'Ordini in corso': return 'Order'
+            case 'Contratti Attivi': return 'Contract'
+        }
+    }
+
+    ordersListcolumns=[
         {label: 'Tipo', fieldName: 'Type', type: 'text'},
         {label: 'Numero Ordine', fieldName: 'OrderNumber', type: 'text'},
         {label: 'Processo', fieldName: 'ProcessType__c', type: 'text'},
@@ -20,30 +31,81 @@ export default class HdtSelezioneFornituraFlow extends SaleVas {
         {label: 'Indirizzo fornitura', fieldName: 'ServicePointAddr', type: 'text'}
     ];
 
-    /* render(){
-        return SaleVasHtml;
-    } */
+    contractsListcolumns = [
+        {label: 'Tipo', fieldName: 'Type', type: 'text'},
+        {label: 'Numero Contratto', fieldName: 'ContractNumber', type: 'text'},
+        {label: 'POD/PDR', fieldName: 'PodPdr', type: 'text'},
+        {label: 'Indirizzo fornitura', fieldName: 'ServicePointAddr', type: 'text'}
+    ];
 
     connectedCallback(){
-        this.handleRadioGroupChange({detail:{value:"Ordini in corso"}});
+        this.handleRadioGroupChange({detail:{value:this.groupOption}});
     }
 
     createTable(data){
         console.log("override");
         data.forEach(item=>{
-            item.Type = 'Ordine';
+            item.Type = this.contextObject;
             item.PodPdr = item.ServicePoint__c !== undefined ? item.ServicePoint__r.ServicePointCode__c : '';
             item.ServicePointAddr = item.ServicePoint__c !== undefined ? item.ServicePoint__r.SupplyAddress__c : '';
         });
         console.log(data)
         super.createTable(data);
     }
-/*
-    getSelectedOrder(event){
-        super.getSelectedOrder(event);
-        console.log(this.selectedOrder);
-        const attributeChangeEvent = new FlowAttributeChangeEvent('selectedOrder', this.selectedOrder);
-        this.dispatchEvent(attributeChangeEvent);
+
+    handleTemporaneaAttivazioneContracts(){
+        this.isLoading = true;
+        getTempNuovaAttivazContracts({accountId:this.accountId}).then(data =>{
+            this.isLoading = false;
+            // this.contractsList = data;
+
+            if(data.length > 0){
+                this.createTable(data);
+            } else {
+                this.showEmptyContractsMessage = true;
+            }
+
+        }).catch(error => {
+            this.isLoading = false;
+            // this.isModalVisible = false;
+            console.log('Error: ', error.body.message);
+            const toastErrorMessage = new ShowToastEvent({
+                title: 'Errore',
+                message: error.body.message,
+                variant: 'error'
+            });
+            this.dispatchEvent(toastErrorMessage);
+        });
     }
-    */
+
+
+
+
+    handleContractsList(){//override Method
+        if(this.groupOption){//condition always true, modify just in case
+            this.handleTemporaneaAttivazioneContracts()
+        }else{//not override method
+            super.handleContractsList();
+        }
+    }
+
+
+
+
+    @api
+    validate() {
+        if(this.ordId || this.contrId || this.template.querySelector("lightning-input").checked) { 
+            return { isValid: true }; 
+        }
+        else { 
+            // If the component is invalid, return the isValid parameter 
+            // as false and return an error message. 
+            return { 
+                isValid: false, 
+                errorMessage: 'Non hai selezionato nessun elemento' 
+            }; 
+        }
+    }
+
+
 }
