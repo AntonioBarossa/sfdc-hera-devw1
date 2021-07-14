6311bea07aef8bf2eeda2541840c4f763e609596
Lorenzo Rollo <lorenzo.rollo@everis.com>
2021-07-14 15:11:45 +0200

    HRADTR-17 recaller KPI on account

--- force-app/main/default/lwc/hdtHighlightsPanel/hdtHighlightsPanel.js
+++ force-app/main/default/lwc/hdtHighlightsPanel/hdtHighlightsPanel.js
@@ -1,4 +1,6 @@
 import { LightningElement, track, api } from 'lwc';
+import updateRecallerCount from '@salesforce/apex/HDT_LC_hdtHighlightsPanel.updateRecallerCount';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 
 const fields = [
     //'ActiveServices__c',  // spostato fuori dal template iterator
@@ -18,7 +20,18 @@ export default class HdtHighlightsPanel extends LightningElement {
         var arraySize = fields.length + 2; // + 2 per i servizi attivi e per il KPI Campagne Attive
         this.gridClass = 'slds-col slds-size_1-of-' + arraySize.toString();
         console.log(this.gridClass);
+        this.doUpdateRecallerCount();
     }
 
-
+    async doUpdateRecallerCount() {
+        const error = await updateRecallerCount({accountId: this.recordId});
+        if(error) {
+            this.dispatchEvent(new ShowToastEvent({
+                variant: 'error',
+                title: 'Errore',
+                message: 'Impossibile calcolare lo stato di Recaller.',
+            }));
+            console.log('### hdtHighlightsPanel.js - ERROR: ' + error);
+        }
+    }
 }
