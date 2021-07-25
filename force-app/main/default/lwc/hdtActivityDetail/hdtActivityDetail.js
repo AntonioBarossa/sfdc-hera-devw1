09f1e94aece30db8fca59ed3d6d6396ffc7c8c1c
Marco Arci <marco.arci@webresults.it>
2021-07-25 15:00:48 +0200

    prelievo dati e stampa (base)

--- force-app/main/default/lwc/hdtActivityDetail/hdtActivityDetail.js
+++ force-app/main/default/lwc/hdtActivityDetail/hdtActivityDetail.js
@@ -1,18 +1,44 @@
-import { LightningElement, api } from 'lwc';
-import getActivityById from '@salesforce/apex/HDT_LC_ActivityDetail.getActivityById';
-import getActivityTemplateById from '@salesforce/apex/HDT_LC_ActivityDetail.getActivityTemplateById';
-import getFieldsTemplateById from '@salesforce/apex/HDT_LC_ActivityDetail.getFieldsTemplateById';
-import getDynamicSectionByFieldsTemplateId from '@salesforce/apex/HDT_LC_ActivityDetail.getDynamicSectionByFieldsTemplateId';
-import getDynamicFieldsByDynamicSectionId from '@salesforce/apex/HDT_LC_ActivityDetail.getDynamicFieldsByDynamicSectionId';
+import { LightningElement, track, api } from 'lwc';
+/*
+    import getActivityById from '@salesforce/apex/HDT_LC_ActivityDetail.getActivityById';
+    import getActivityTemplateById from '@salesforce/apex/HDT_LC_ActivityDetail.getActivityTemplateById';
+    import getFieldsTemplateById from '@salesforce/apex/HDT_LC_ActivityDetail.getFieldsTemplateById';
+    import getDynamicSectionByFieldsTemplateId from '@salesforce/apex/HDT_LC_ActivityDetail.getDynamicSectionByFieldsTemplateId';
+    import getDynamicFieldsByDynamicSectionId from '@salesforce/apex/HDT_LC_ActivityDetail.getDynamicFieldsByDynamicSectionId';
+*/
+import getDetail from '@salesforce/apex/HDT_LC_ActivityDetail.getDetail';
+
 export default class HdtActivityDetail extends LightningElement {
     @api recordId;
-
+    @track detail;
     connectedCallback(){
-        const activity = getActivityById({"idActivity":this.recordId});
-        const activityTemplate= getActivityTemplateById({"idActivityTemplate":this.activity.wrts_prcgvr__ActivityTemplate__c});
-        const fieldsTemplate= getFieldsTemplateById({"idFieldsTemplate":this.activityTemplate.wrts_prcgvr__FieldsTemplate__c});
-        const dynamicSection= getDynamicSectionByFieldsTemplateId({"idFieldsTemplate":this.fieldsTemplate.Id});
-        const dynamicFields= getDynamicFieldsByDynamicSectionId({"idDynamicSection":this.dynamicSection.Id});
+        /*
+            const activity = getActivityById({"idActivity":this.recordId});
+            const activityTemplate= getActivityTemplateById({"idActivityTemplate":this.activity.wrts_prcgvr__ActivityTemplate__c});
+            const fieldsTemplate= getFieldsTemplateById({"idFieldsTemplate":this.activityTemplate.wrts_prcgvr__FieldsTemplate__c});
+            const dynamicSection= getDynamicSectionByFieldsTemplateId({"idFieldsTemplate":this.fieldsTemplate.Id});
+            const dynamicFields= getDynamicFieldByDynamicSectionId({"idDynamicSection":this.dynamicSection.Id});
+        */
+       this.loadContext();
     }
+
+    async loadContext(){
+        this.detail = await getDetail({"idActivity":this.recordId});
+        this.handleLoad();
+        this.show=true;
+    }
+
+    @track show=false;
+    @track mapData = [];
+    async handleLoad(){
+        for(var key in this.detail){
+            var arr=[];
+            Array.prototype.forEach.call(this.detail[key], val => {
+                arr.push(val.Id);
+            });
+            this.mapData.push({value:arr, key:key});
+        }
+    }
+    
 
 }
