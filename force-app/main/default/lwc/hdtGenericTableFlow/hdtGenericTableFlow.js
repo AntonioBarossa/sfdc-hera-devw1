9f3da368f33648601f675038fb4de378562d2b35
Francesco Panico <frpanico@everis.com>
2021-07-26 15:26:28 +0200

    [HRADTR-482] Contratto Non Richiesto

--- force-app/main/default/lwc/hdtGenericTableFlow/hdtGenericTableFlow.js
+++ force-app/main/default/lwc/hdtGenericTableFlow/hdtGenericTableFlow.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class HdtGenericTableFlow extends LightningElement {}
+export default class HdtGenericTableFlow extends LightningElement {
+
+//Flow Inputs
+    //buttons
+    @api availableActions = [];
+    @api saveButton;
+    @api cancelButton;
+    @api previousButton;
+    @api draftButton;
+	@api labelSaveButton;
+    @api labelDraftButton;
+    @api labelPreviousButton;
+    @api labelCancelButton;
+    @api draft = false;
+    @api cancel = false;
+    //utilities
+    @api tableName;
+    @api searchKey;
+    @api searchTerm;
+    @api maxRowSelect;
+
+    handlePrevious(){
+        const navigateBackEvent = new FlowNavigationBackEvent();
+        this.dispatchEvent(navigateBackEvent);
+    }
+    handleNext(event) {
+        if(event.target.name === 'draft'){
+            this.draft = true;
+        } else if(event.target.name == 'cancel'){
+            this.cancel = true;
+        }
+        if(this.availableActions.find(action => action === 'NEXT')){    
+            const navigateNextEvent = new FlowNavigationNextEvent();
+            this.dispatchEvent(navigateNextEvent);
+        } else {
+            const navigateFinish = new FlowNavigationFinishEvent();
+            this.dispatchEvent(navigateFinish);
+        }
+    }
+}
