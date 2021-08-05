c526f31b44ff13e50190c0aadaed77c014026e0c
Enrico Capozzo <enrico.capozzo@webresults.it>
2021-08-05 14:46:48 +0200

    Bug1725

--- force-app/main/default/aura/HDT_LCP_CampaignToAccountList/HDT_LCP_CampaignToAccountListController.js
+++ force-app/main/default/aura/HDT_LCP_CampaignToAccountList/HDT_LCP_CampaignToAccountListController.js
@@ -1,28 +1,44 @@
 ({
     doInit : function(component, event, helper) {
-        console.log(component.get("v.recordId"));
-        console.log(component.get("v.sObjectName"));
-        //$A.enqueueAction(component.get('c.loadOpenCamp'));
+     
+            console.log(component.get("v.recordId"));
+            console.log(component.get("v.sObjectName"));
+            var workspaceAPI = component.find("workspace");
+
+            workspaceAPI.getEnclosingTabId().then(function(ctabId) {
+                console.log(ctabId);
+                component.set("v.currentTab", ctabId);
+                
+                
+            })
+  
     },
     
     onTabClosed : function(component, event, helper) {
         var tabId = event.getParam('tabId'); 
         console.log("Tab closed: " + tabId);
+        console.log("Tab Current: " +component.get("v.currentTab"));
         console.log(component.get("v.recordId"));
         
         var workspaceAPI = component.find("workspace");
-
-        workspaceAPI.openTab({
-            url: '/lightning/cmp/c__HDT_LCP_Confirmation?c__varId='+component.get("v.recordId"),
-            focus: true
-        }).then(function(response) {
-              // actions to do after opening tab if necessary
-              console.log(response);
-        }).catch(function(error) {
-            console.log(error);
-        });
-
+        
+        
+        
+        if (tabId==component.get("v.currentTab")) {
+            
+            
+            
+            workspaceAPI.openTab({
+                url: '/lightning/cmp/c__HDT_LCP_Confirmation?c__varId='+component.get("v.recordId"),
+                focus: true
+            }).then(function(response) {
+                // actions to do after opening tab if necessary
+                console.log(response);
+            }).catch(function(error) {
+                console.log(error);
+            });
+        }
         
     },
- 
+    
 })
