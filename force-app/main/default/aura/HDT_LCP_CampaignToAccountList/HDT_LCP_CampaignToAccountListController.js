abc48068c1a9b7774c8e907c28e5b43993f99f71
Enrico Capozzo <enrico.capozzo@webresults.it>
2021-08-05 13:31:19 +0200

    Bug1725

--- force-app/main/default/aura/HDT_LCP_CampaignToAccountList/HDT_LCP_CampaignToAccountListController.js
+++ force-app/main/default/aura/HDT_LCP_CampaignToAccountList/HDT_LCP_CampaignToAccountListController.js
@@ -2,11 +2,27 @@
     doInit : function(component, event, helper) {
         console.log(component.get("v.recordId"));
         console.log(component.get("v.sObjectName"));
+        //$A.enqueueAction(component.get('c.loadOpenCamp'));
     },
-
+    
     onTabClosed : function(component, event, helper) {
         var tabId = event.getParam('tabId'); 
         console.log("Tab closed: " + tabId);
-        component.find('hdtCampaignToAccountList').updateCampaignMemberStatus();
-    }
+        console.log(component.get("v.recordId"));
+        
+        var workspaceAPI = component.find("workspace");
+
+        workspaceAPI.openTab({
+            url: '/lightning/cmp/c__HDT_LCP_Confirmation?c__varId='+component.get("v.recordId"),
+            focus: true
+        }).then(function(response) {
+              // actions to do after opening tab if necessary
+              console.log(response);
+        }).catch(function(error) {
+            console.log(error);
+        });
+
+        
+    },
+ 
 })
---
+++ force-app/main/default/aura/HDT_LCP_Confirmation/HDT_LCP_ConfirmationController.js
@@ -0,0 +1,155 @@
+({
+    
+    doInit : function(component, event, helper) {
+        console.log('doinit');
+        setTimeout(function(){ 
+            
+            
+            
+            var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            testParam = '';
+            var varId;
+            
+            
+            
+            for(let i = 0; i < sURLVariables.length; i++){
+                
+                testParam = '';
+                testParam = sURLVariables[i].split('=');
+                
+                
+                if (testParam[i] == 'c__varId'){
+                    console.log(testParam[i]);
+                    console.log(testParam[i+1]);
+                    varId = testParam[i+1];
+                }
+                
+            }
+            component.set("v.varId", varId);
+            
+            var workspaceAPI = component.find("workspace");
+            
+            workspaceAPI.getFocusedTabInfo().then(function(response) {
+                var focusedTabId = response.tabId;
+                component.set("v.varTabId", focusedTabId);
+                workspaceAPI.disableTabClose({
+                    tabId: focusedTabId,
+                    disabled: true
+                })
+                .then(function(tabInfo) {
+                    console.log(tabInfo);
+                    
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        }, 200);
+        
+        
+    },
+    onTabClosed : function(component, event, helper) {
+        
+        
+        
+    },
+    
+    
+    
+    closedNow : function(component, event, helper) {
+        component.set('v.loaded', true);
+        console.log('varID : '+component.get("v.varId"));
+        
+        $A.enqueueAction(component.get('c.loadClosedCamp'));
+        var workspaceAPI = component.find("workspace");
+        
+        workspaceAPI.getFocusedTabInfo().then(function(response) {
+            var focusedTabId = response.tabId;
+            workspaceAPI.disableTabClose({
+                tabId: focusedTabId,
+                disabled: false
+            })
+            .then(function(tabInfo) {
+                console.log(tabInfo);
+                
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+        
+        
+        
+        
+    },
+    
+    goBack: function(component, event, helper) {
+        var workspaceAPI = component.find("workspace");
+        
+        
+     
+        },
+        
+        loadClosedCamp: function (component, event, helper) {
+            console.log('loadClosedCamp');
+            
+            
+            var action = component.get("c.methodClosedTab");
+            action.setParams({
+                "id":component.get("v.varId"),
+                "objectName":"Account",
+                "category":"Campagna CRM",
+                "channel":""
+            })
+            
+            action.setCallback(this, function(response) {
+                
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    
+                    console.log(state + 'return : '+response.getReturnValue());
+                    setTimeout(function(){ 
+                        var workspaceAPI = component.find("workspace");
+                        
+                        workspaceAPI.getFocusedTabInfo().then(function(response) {
+                            var focusedTabId = response.tabId;
+                            workspaceAPI.closeTab({tabId: focusedTabId});
+                        })
+                        .catch(function(error) {
+                            console.log(error);
+                        });    }, 200);
+                        if (response.getReturnValue()) {
+                            component.set('v.loaded', false);
+                            
+                            $A.enqueueAction(component.get('c.showToast'));
+                            
+                        }
+                        
+                    } 
+                    else {
+                        console.log(state);
+                    }
+                });
+                $A.enqueueAction(action);
+                
+            },
+            
+            showToast : function(component, event, helper) {
+                var toastEvent = $A.get("e.force:showToast");
+                component.set("v.reopen", false); 
+                toastEvent.setParams({
+                    "variant": "Success",
+                    "title": "Success!",
+                    "message": "Stato impostato a \"Non Proposto Auto\""
+                });
+                toastEvent.fire();
+            }
+        })
+        
