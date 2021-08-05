0e47d8907c1362d2fb0d3eac6a3aa47033fef956
Enrico Capozzo <enrico.capozzo@webresults.it>
2021-08-05 21:02:43 +0200

    conferma chiusura tab

--- force-app/main/default/aura/HDT_LCP_Confirmation/HDT_LCP_ConfirmationController.js
+++ force-app/main/default/aura/HDT_LCP_Confirmation/HDT_LCP_ConfirmationController.js
@@ -92,6 +92,35 @@
             $A.enqueueAction(action);
             
         },
+        goBack: function (component, event, helper) {
+            var workspaceAPI = component.find("workspace");
+           
+        var workspaceAPI = component.find("workspace");
+        workspaceAPI.getFocusedTabInfo().then(function(response) {
+            var focusedTabId = response.tabId;
+            workspaceAPI.closeTab({tabId: focusedTabId});
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+        workspaceAPI.openTab({
+            url: '/lightning/r/Account/'+component.get("v.varId")+'/view',
+            focus: true
+        }).then(function(response) {
+            workspaceAPI.getTabURL({
+                tabId: response
+            }).then(function(response) {
+                console.log(response);
+            });
+       })
+        .catch(function(error) {
+            console.log(error);
+        });
+    
+
+     
+        },
+
         
         showToast : function(component, event, helper) {
             var toastEvent = $A.get("e.force:showToast");
