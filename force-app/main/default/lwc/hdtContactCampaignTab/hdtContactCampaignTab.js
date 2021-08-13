5dab70aa43a4b5fcd1b2c3cab73ec29954c5aa58
Enrico Capozzo <enrico.capozzo@webresults.it>
2021-08-13 19:35:58 +0200

    1733-Campagne CRM

--- force-app/main/default/lwc/hdtContactCampaignTab/hdtContactCampaignTab.js
+++ force-app/main/default/lwc/hdtContactCampaignTab/hdtContactCampaignTab.js
@@ -4,17 +4,24 @@ import { NavigationMixin } from 'lightning/navigation';
 
 export default class HdtContactCampaignTab extends NavigationMixin(LightningElement) {
     @api campaignmember;
+    @api noredirect=false;
     @track currentId;
     genericRedirect(event){
-        console.log(event.currentTarget.dataset.id);
-        console.log(event.currentTarget.dataset.name);
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: event.currentTarget.dataset.id,
-                objectApiName: event.currentTarget.dataset.name,
-                actionName: 'view'
-            },
-        });
+        if (!noRedirect) {
+            console.log(event.currentTarget.dataset.id);
+            console.log(event.currentTarget.dataset.name);
+            this[NavigationMixin.Navigate]({
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId: event.currentTarget.dataset.id,
+                    objectApiName: event.currentTarget.dataset.name,
+                    actionName: 'view'
+                },
+            });
+        }
+        
+        
+        
+        
     }
 }
