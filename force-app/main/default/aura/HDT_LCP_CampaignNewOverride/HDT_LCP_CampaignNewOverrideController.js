293b865a2481d91811824fb8e8582154274c9ed6
Andrei Necsulescu <andrei.necsulescu@webresults.it>
2021-08-13 12:35:46 +0200

    Restored modal closing logic

--- force-app/main/default/aura/HDT_LCP_CampaignNewOverride/HDT_LCP_CampaignNewOverrideController.js
+++ force-app/main/default/aura/HDT_LCP_CampaignNewOverride/HDT_LCP_CampaignNewOverrideController.js
@@ -12,16 +12,8 @@
     },
 
     cancelDialog: function (component, event, helper) {
-        // var workspaceAPI = component.find("workspace");
-        // workspaceAPI.closeTab({ tabId: component.get('v.tabId') });
         var workspaceAPI = component.find("workspace");
-        workspaceAPI.getFocusedTabInfo().then(function(response) {
-            var focusedTabId = response.tabId;
-            workspaceAPI.closeTab({tabId: focusedTabId});
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+        workspaceAPI.closeTab({ tabId: component.get('v.tabId') });
     },
 
     saveRecord: function (component, event, helper) {
