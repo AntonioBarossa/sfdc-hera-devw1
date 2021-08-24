762034964e35b1cd1c04edab21563f2efb53ee21
Andrea Arosio <andrea.arosio@webresults.it>
2021-08-24 15:36:36 +0200

    Aggiunti al cmp hdtSupplySelection i campi serviceRequestID e isCompatible

--- force-app/main/default/lwc/hdtSupplySelection/hdtSupplySelection.js
+++ force-app/main/default/lwc/hdtSupplySelection/hdtSupplySelection.js
@@ -21,6 +21,8 @@ export default class hdtSupplySelection extends LightningElement {
     @api outputContract;
     @api responseArriccData;
     @api isRicercainSAP=false;
+    @api isCompatible;
+    @api serviceRequestId;
 
 
     /**
@@ -146,6 +148,8 @@ export default class hdtSupplySelection extends LightningElement {
      * Get selected service point
      */
     handleServicePointSelection(event){
+        this.isCompatible = event.detail.isCompatible;
+        this.serviceRequestId = event.detail.serviceRequestId;
         console.log('handleServicePointSelection' + JSON.stringify(event.detail));
         this.selectedServicePoint = event.detail;
         let contractNumber = this.selectedServicePoint['Contract Number'];
