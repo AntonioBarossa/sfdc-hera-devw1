b8fb6f3f23af2f071f7ebf5b271ea8b9aa8492c1
Gabriele <gabriele.rota@webresults.it>
2021-08-25 15:23:14 +0200

    switch flag resident

--- force-app/main/default/lwc/hdtTargetObjectCreateForm/hdtTargetObjectCreateForm.js
+++ force-app/main/default/lwc/hdtTargetObjectCreateForm/hdtTargetObjectCreateForm.js
@@ -313,14 +313,19 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
                         }
                     ) 
                 }
-                else if(this.recordtype.label === 'Punto Elettrico' && element === 'Resident__c'){
+                else if(element === 'Resident__c'){
+
                     console.log('entra in resident');
-                    this.allSubmitedFields.Resident__c = true;
+                    
+                    //25/08/2021 - gabriele.rota@webresults.it - Switch Flag Resident in base a Tipo Fornitura
+                    let residentFlag = (this.allSubmitedFields.SupplyType__c=='Domestico' || this.allSubmitedFields.SupplyType__c=='A-Domestico');
+
+                    this.allSubmitedFields.Resident__c = residentFlag;
                     fieldsDataObject.push(
                         {
                             fieldname: element,
                             required : mapFieldReq.get(element),
-                            value:true,
+                            value: residentFlag,
                             disabled: false
                         }
                     ) 
@@ -790,6 +795,12 @@ export default class HdtTargetObjectCreateForm extends LightningElement {
 
     
         }
+
+        //25/08/2021 - gabriele.rota@webresults.it - Switch Flag Resident in base a Tipo Fornitura
+        if(event.target.fieldName =='SupplyType__c'){
+            this.fieldsDataObject = this.toObject(this.fieldsData, this.fieldsDataReq);
+        }
+        
         console.log('handleFieldsDataChange END');
        
 
