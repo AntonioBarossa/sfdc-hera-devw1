5a191e004531fbc5ba7f96d5fdfa1df5233f65c2
Enrico Capozzo <enrico.capozzo@webresults.it>
2021-09-17 11:37:39 +0200

    HRAWRM-621

--- force-app/main/default/lwc/hdtCreateCampaign/hdtCreateCampaign.js
+++ force-app/main/default/lwc/hdtCreateCampaign/hdtCreateCampaign.js
@@ -8,6 +8,7 @@ import USER_ID from '@salesforce/user/Id';
 export default class HdtCreateCampaign extends LightningElement {
     @api recordId;
     objectApiName = 'Campaign';
+    reqEndDate=false;
     requiredShippingMethods=false;  // Start HRAWRM-621 16/09/2021
     @track reitekFieldRequired = false;
     @track startDateFieldRequired = false;
@@ -91,6 +92,8 @@ export default class HdtCreateCampaign extends LightningElement {
         let campReq = this.template.querySelector("lightning-input-field[data-id=req]").value
         console.log('campReq' +campReq);
         this.reqPrioritycheck(campReq);
+        this.checkRequiredShippingMethods(categoryField,channelField,this.statusField);  // Start HRAWRM-621 16/09/2021
+
         if ( this.statusField!='Bozza' && channelField=='Telefonico Outbound' ) {
             this.easyRequired=true;
         }
@@ -154,14 +157,17 @@ export default class HdtCreateCampaign extends LightningElement {
         }
     }
     // Start HRAWRM-621 16/09/2021
-    checkRequiredShippingMethods(category,channel){
-
-        if ( category === 'Campagna Outbound' && channel==='Cartaceo' ) {
+    checkRequiredShippingMethods(category,channel,status){
+        console.log('category: '+category);
+        console.log('channel: '+channel);
+        console.log('status: '+status);
+        if ( category == 'Campagna Outbound' && channel=='Cartaceo' && status!='Bozza') {
             this.requiredShippingMethods=true;
         }
         else{
             this.requiredShippingMethods=false;
         }
+        console.log('requiredShippingMethods:'+this.requiredShippingMethods);
     }
     // End HRAWRM-621 16/09/2021
 
@@ -187,7 +193,7 @@ export default class HdtCreateCampaign extends LightningElement {
         }
         this.paperCampaignFields = event.detail.value.includes('Cartaceo') ? true : false;
         let categoryField=this.template.querySelector('.categoryField > lightning-input-field').value
-        this.checkRequiredShippingMethods(categoryField,event.detail.value);  // Start HRAWRM-621 16/09/2021
+        this.checkRequiredShippingMethods(categoryField,event.detail.value,this.statusField);  // Start HRAWRM-621 16/09/2021
         //reset fields
 
     }
