463ba49a7e191fc0ebb0a28fb2fad0598f769345
Elvis Angelaccio <elvis.angelaccio@everis.com>
2021-09-25 20:49:19 +0200

    Fix function

--- force-app/main/default/lwc/hdtDocumentSignatureManagerFlow/hdtDocumentSignatureManagerFlow.js
+++ force-app/main/default/lwc/hdtDocumentSignatureManagerFlow/hdtDocumentSignatureManagerFlow.js
@@ -445,7 +445,7 @@ export default class HdtDocumentSignatureManagerFlow extends NavigationMixin(Lig
                 }).then(result => {
                     this.handleGoNext();
                 }).catch(error => {
-                    this.showToast('Errore nell\'invio del documento al cliente.');
+                    this.showMessage('Errore nell\'invio del documento al cliente.','error');
                     console.error(error);
                 });
             }
