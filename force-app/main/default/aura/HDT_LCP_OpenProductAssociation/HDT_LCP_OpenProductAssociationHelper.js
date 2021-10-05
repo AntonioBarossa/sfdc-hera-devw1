fe263b3419c5b69d3a8d069b1fe28814ec06476f
dmontors <daniele.montorselli@everis.com>
2021-10-05 14:37:39 +0200

    [HRADTR-325] group member

--- force-app/main/default/aura/HDT_LCP_OpenProductAssociation/HDT_LCP_OpenProductAssociationHelper.js
+++ force-app/main/default/aura/HDT_LCP_OpenProductAssociation/HDT_LCP_OpenProductAssociationHelper.js
@@ -21,6 +21,30 @@
 		navService.navigate(pageReference);
 		$A.get("e.force:closeQuickAction").fire();
 
+	},
+
+	getEnabledUser: function(component, event, helper) {
+		console.log('# getEnabledUser #');
+
+        var action = component.get("c.getEnabledUser");
+        //action.setParams({
+        //    productId: productId
+        //});
+
+        action.setCallback(this, function (response) {
+            var returnObj = response.getReturnValue();
+			console.log('# returnObj: ' + returnObj);
+
+			if(returnObj){
+				this.initHelperMethod(component, event, helper);
+			} else {
+				component.set('v.enabled', false);
+				console.log('# Your user don\'t have permission');
+			}
+
+
+        });
+        $A.enqueueAction(action);
 	}
 
 })
