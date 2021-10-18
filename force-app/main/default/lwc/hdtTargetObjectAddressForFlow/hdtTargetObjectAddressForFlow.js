e47cbe1292b6570ba1389ed50cf63f03b66502e7
fdefelice <federico.defelice@webresults.it>
2021-10-18 13:00:31 +0200

    fix varie

--- force-app/main/default/lwc/hdtTargetObjectAddressForFlow/hdtTargetObjectAddressForFlow.js
+++ force-app/main/default/lwc/hdtTargetObjectAddressForFlow/hdtTargetObjectAddressForFlow.js
@@ -117,9 +117,11 @@ export default class HdtTargetObjectAddressForFlow extends LightningElement {
         if(this.theCase["InvoicingStreetNumber__c"] != undefined){
             wrapperAddress['Civico'] = this.theCase["InvoicingStreetNumber__c"];
         }
-        wrapperAddress["AbilitaVerifica"]=false;
-        //wrapperAddress["Flag Verificato"]=true;
-        //wrapperAddress["FlagVerificato"]=true;
+        //wrapperAddress["AbilitaVerifica"]=false;//abilita il tasto verifica
+        if(this.validateAddress(wrapperAddress)?.isValid){
+            wrapperAddress["Flag Verificato"]=true;//questi due abilitano la check "verificata"
+            wrapperAddress["FlagVerificato"]=true;//questi due abilitano la check "verificata"
+        }
         console.log('wrapper addr'+JSON.stringify(wrapperAddress));
         const targetFields = this.template.querySelector("c-hdt-target-object-address-fields");
         //getInstanceWrapObject()
