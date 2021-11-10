b3e983864d95784dd103503b93b726a6c8553a11
dmontors <daniele.montorselli@everis.com>
2021-11-10 17:33:25 +0100

    [HRADTR-14] meters list metadata

--- force-app/main/default/lwc/hdtMetersListOnServicePoint/hdtMetersListOnServicePoint.js
+++ force-app/main/default/lwc/hdtMetersListOnServicePoint/hdtMetersListOnServicePoint.js
@@ -1,4 +1,4 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import getDatatableStructure from '@salesforce/apex/HDT_LC_MetersList.getDatatableStructure';
 import getDataInContinuation from '@salesforce/apexContinuation/HDT_LC_MetersList.startRequest';
 
@@ -8,8 +8,11 @@ export default class HdtMetersListOnServicePoint extends LightningElement {
     spinner = true;
     error;
     errorMessage = '';
+    totalResult = 0;
     columns = [];
     data = [];
+    tableHeightCss;
+    title = 'Elenco Misuratori';
 
     connectedCallback(){
         console.log('>>> recordId: ' + this.recordId);
@@ -45,6 +48,35 @@ export default class HdtMetersListOnServicePoint extends LightningElement {
             this.error = false;
             var parsedResult = JSON.parse(result);
             this.data = parsedResult.data;
+
+            /*
+            this.data.push({assetNumber: '0000000000000001', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000003', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000004', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000005', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000006', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000007', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000008', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000880', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000888', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000555', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000044', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000333', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000222', materialDescription: 'Device Inforecord'});
+            this.data.push({assetNumber: '0000000000000999', materialDescription: 'Device Inforecord'});
+            */
+
+            this.totalResult = parsedResult.data.length;
+            console.log('>>> this.data.length ' + this.data.length);
+
+            this.title = 'Elenco Misuratori (' + this.totalResult.toString() + ')';
+
+            if(this.totalResult > 10){
+                this.tableHeightCss = 'full-table';
+            } else {
+                this.tableHeightCss = 'short-table';
+            }
+
             this.spinner = false;
         })
         .catch(error => {
