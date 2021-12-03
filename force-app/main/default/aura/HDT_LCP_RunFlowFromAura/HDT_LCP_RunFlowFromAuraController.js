fd37bec175d9db40b5cb4e6e270635e56e3f2c27
SIMONE LONGHI <simone.longhi.st@nttdata.com>
2021-12-03 10:55:36 +0100

    [HRDTR_SLONGHI] Fix error

--- force-app/main/default/aura/HDT_LCP_RunFlowFromAura/HDT_LCP_RunFlowFromAuraController.js
+++ force-app/main/default/aura/HDT_LCP_RunFlowFromAura/HDT_LCP_RunFlowFromAuraController.js
@@ -224,36 +224,38 @@
                     "target":null,
                     "currentTarget":null}
                 */
-                console.log('Inside Error condition: ' + JSON.stringify(event));
-
-                var action = component.get("c.isMandatoryComplete");
-                action.setParams({
-                    "recordid" : newCaseId 
-                });
-                action.setCallback(this, function(response) {
-                    var state = response.getState();
-                    if (state === "SUCCESS") {
-                        component.set("v.thereIsActivity", response.getReturnValue());
-                    }
-                });
-                $A.enqueueAction(action);
-
-                if( !component.get('v.thereIsActivity') ){
-                    var toastEvent = $A.get("e.force:showToast");
-                    toastEvent.setParams({
-                        "title": "Errore",
-                        "message": "Ci sono attività obbligatorie da completare.",
-                        "type" : "error"
+                    console.log('Inside Error condition: ' + JSON.stringify(event));
+    
+                    var action = component.get("c.isMandatoryComplete"); //start call
+                    action.setParams({
+                        "recordid" : component.get('v.recordid') 
                     });
-                    toastEvent.fire();
-                } else {
-                    var toastEvent = $A.get("e.force:showToast");
-                    toastEvent.setParams({
-                        "title": "Errore",
-                        "message": "Non è stato possibile portare a termine le operazioni.\nSi prega di contattare l'Amministratore di sistema",
-                        "type" : "error"
+                    action.setCallback(this, function(response) {
+                        var state = response.getState();
+                        if (state === "SUCCESS") {
+                            component.set("v.thereIsActivity", response.getReturnValue());
+                        }
+                        if( !component.get('v.thereIsActivity') ){  // error "management"
+                            var toastEvent = $A.get("e.force:showToast"); // activities-to-complete error
+                            toastEvent.setParams({
+                                "title": "Errore",
+                                "message": "Ci sono attività obbligatorie da completare.",
+                                "type" : "error"
+                            });
+                            toastEvent.fire();
+                        } else {                                    // standard error
+                            var toastEvent = $A.get("e.force:showToast");
+                            toastEvent.setParams({
+                                "title": "Errore",
+                                "message": "Non è stato possibile portare a termine le operazioni.\nSi prega di contattare l'Amministratore di sistema",
+                                "type" : "error"
+                            });
+                            toastEvent.fire();
+                        }
                     });
-                    toastEvent.fire();
+                    $A.enqueueAction(action);
+
+                    console.log('# thereIsActivity >>>> ' + component.get('v.thereIsActivity') );
                 }
             }
             
