14752d74f114bfae5705fbf985170bbc9110e6b1
AndreiNecsulescu <andrei.necsulescu@webresults.it>
2021-12-22 11:00:48 +0100

    [Commit Automatico]

--- force-app/main/default/lwc/hdtSupplySelectionFlow/hdtSupplySelectionFlow.js
+++ force-app/main/default/lwc/hdtSupplySelectionFlow/hdtSupplySelectionFlow.js
@@ -20,40 +20,31 @@ export default class HdtSupplySelectionFlow extends LightningElement {
     saveDisabled = true;
 
     handleServicePointSelection(event){
-        console.log('SUPPLYSELECTIONFLOW_event ' +JSON.stringify(event.detail));
+
         this.selectedServicePointFlow = event.detail["Codice Punto"];
         this.serviceRequestId = event.detail["serviceRequestId"];
         this.isCompatible = event.detail["isCompatible"];
-        console.log('isCompatibile ' + this.isCompatible);
-        if(this.isCompatible === false)
-        {
+
+        if(this.isCompatible === false){
             this.cancelDisabled = false;
             this.cancelCase = true;
         }
-        else if(this.isCompatible === true)
-        {
+        else if(this.isCompatible === true){
             this.saveDisabled = false;
         }
-        console.log('forn '+ this.selectedServicePointFlow);
     }
 
     handleConfirmServicePoint(event){
-        console.log('forn '+ this.selectedServicePointFlow);
-        console.log('SUPPLYSELECTIONFLOW_IsCompatible ' + this.isCompatible);
         this.handleSave(event);
     }
+
     handleSave(event){
-
         const navigateNextEvent = new FlowNavigationNextEvent();
-
         this.dispatchEvent(navigateNextEvent);
-
     }
+
     handleCancell(event){
-
         this.cancelCase = true;
-
         this.handleSave();
-        
     }
 }
