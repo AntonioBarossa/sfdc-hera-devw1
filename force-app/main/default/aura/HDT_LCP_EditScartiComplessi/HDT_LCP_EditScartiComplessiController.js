005e3c4dacee71240757f33f57836c283da486a1
Michele Argentino <michele11.argentino@nttdata.com>
2022-02-03 17:13:04 +0100

    Gestion scarti - sales

--- force-app/main/default/aura/HDT_LCP_EditScartiComplessi/HDT_LCP_EditScartiComplessiController.js
+++ force-app/main/default/aura/HDT_LCP_EditScartiComplessi/HDT_LCP_EditScartiComplessiController.js
@@ -9,7 +9,12 @@
             if (response.getState() === "SUCCESS"){
                 console.log('@@@data ' + response.getReturnValue());
                 let data = JSON.parse(response.getReturnValue());
-                if (data.objectType === "Case"){
+                if(data.objectType === "wrts_prcgvr__Activity__c"){
+                    console.log('@@@In Activity');
+                    component.set('v.activity', data.object);
+                    helper.openWizardForActivity(component,event);
+                }
+                else if (data.objectType === "Case"){
                     console.log('@@@In case');
                     helper.openWizardForCase(component,event);
                 }else{
@@ -21,7 +26,7 @@
                 console.log('@@@@ERROR ');
                 var errors = response.getError();
                 if (errors && errors[0] && errors[0].message) {
-                    helper.showAllert(component,errors[0].message,'error','Attenzione!');
+                    helper.showAlert(component,errors[0].message,'error','Attenzione!');
                     $A.get("e.force:closeQuickAction").fire();
                 }
             }
