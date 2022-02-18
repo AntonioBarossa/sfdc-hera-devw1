7622c19bb8490793e444e42e4811c3ac87f4fd83
emanalpi <emanalpi@everis.com>
2022-02-18 12:15:14 +0100

    fix credit Check

--- force-app/main/default/lwc/hdtChildOrderProcessPrecheck/hdtChildOrderProcessPrecheck.js
+++ force-app/main/default/lwc/hdtChildOrderProcessPrecheck/hdtChildOrderProcessPrecheck.js
@@ -34,6 +34,7 @@ export default class hdtChildOrderProcessPrecheck extends LightningElement {
     service = '';
     pickValue = '';
     causaleContendibilita = '';
+    creditCheck = false;
 
     get isNotBillable(){
         return this.order.RecordType.DeveloperName === 'HDT_RT_VAS' && !this.order.IsBillableVas__c;
@@ -334,6 +335,10 @@ export default class hdtChildOrderProcessPrecheck extends LightningElement {
         //EVERIS
 
         //EVERIS: Aggiunta variabile Order
+        //Questo pasaggio è un passaggio obbligatorio altrimenti va in errore durante l'assegnazione.
+        this.order = JSON.parse(JSON.stringify(this.order));
+        this.order.CreditCheckInProgress__c = this.creditCheck;
+
         next({order: this.order,orderId: this.order.Id, selectedProcessObject: this.selectedProcessObject, deliberate: this.deliberation, extraParams: extraParams, srRequest: this.serviceRequest}).then(data =>{
             if(data != '' && data != this.order.OrderNumber){
                 const toastErrorMessage = new ShowToastEvent({
@@ -375,7 +380,7 @@ export default class hdtChildOrderProcessPrecheck extends LightningElement {
          console.log('****12');
         //if((this.selectedProcessObject.recordType === 'HDT_RT_VAS' && (this.order.OrderReferenceNumber == null || this.order.OrderReferenceNumber === undefined) && (this.order.ContractReference__c == null || this.order.ContractReference__c === undefined)) || (['HDT_RT_Voltura', 'HDT_RT_Subentro', 'HDT_RT_AttivazioneConModifica', 'HDT_RT_ConnessioneConAttivazione', 'HDT_RT_TemporaneaNuovaAtt', 'HDT_RT_SwitchIn', 'HDT_RT_Attivazione'].includes(this.selectedProcessObject.recordType) && this.selectedProcessObject.processType != 'Switch in Ripristinatorio')){
         if((this.selectedProcessObject.recordType === 'HDT_RT_VAS' && this.order.SBQQ__Quote__c != this.order?.OrderReference__r?.SBQQ__Quote__c ) || (['HDT_RT_Voltura','HDT_RT_VolturaConSwitch','HDT_RT_Subentro', 'HDT_RT_AttivazioneConModifica', 'HDT_RT_ConnessioneConAttivazione', 'HDT_RT_TemporaneaNuovaAtt', 'HDT_RT_SwitchIn', 'HDT_RT_Attivazione'].includes(this.selectedProcessObject.recordType) && this.selectedProcessObject.processType != 'Switch in Ripristinatorio')){
-            this.order.CreditCheckInProgress__c = true;
+            this.creditCheck = true;
             this.callCreditCheckSAP();
         }
         console.log('****13');
@@ -515,10 +520,6 @@ export default class hdtChildOrderProcessPrecheck extends LightningElement {
             }
         ];
 
-        console.log('#Calling Credit Check......');
-        this.callCreditCheckSAP();
-        console.log('#End Credit Check......');
-        console.log('CallBack end');
     }
 
     typeVisibility(type){
