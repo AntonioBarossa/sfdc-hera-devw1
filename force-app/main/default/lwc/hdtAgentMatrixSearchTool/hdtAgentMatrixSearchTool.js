6c6858cea63c8299ae64c269d26c336fcad81d0a
Lorenzo Rollo <lorenzo.rollo@everis.com>
2022-02-20 18:01:22 +0100

    HRADTR-480 completed AgentMatrixSearchTool cmp

--- force-app/main/default/lwc/hdtAgentMatrixSearchTool/hdtAgentMatrixSearchTool.js
+++ force-app/main/default/lwc/hdtAgentMatrixSearchTool/hdtAgentMatrixSearchTool.js
@@ -1,4 +1,5 @@
 import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getCurrentAgent from '@salesforce/apex/HDT_LC_AgentMatrixSearchToolController.getCurrentAgent';
 import updateRule from '@salesforce/apex/HDT_LC_AgentMatrixSearchToolController.updateRule';
 import getAgents from '@salesforce/apex/HDT_LC_AgentMatrixSearchToolController.getAgents';
@@ -23,29 +24,40 @@ export default class HdtAgentMatrixSearchTool extends LightningElement {
     async connectedCallback() {
         this.selectedAgent = await getCurrentAgent({ruleId: this.recordId});
     }
-    
+
     async search(event) {
         if(event.keyCode === 13 && event.target.value) {
             try {
-                console.log(event.target.value);
                 this.agents = await getAgents({queryString: event.target.value});
             } catch(e) {
-                console.log(e);
+                console.error(e);
+                this.showToast('Errore','Si è verificato un errore. Contattare il supporto tecnico.','error');
             }
         }
     }
-    
+
     async setAgent(event) {
-        var res = await updateRule({ruleId: this.recordId, agentCode: event.target.value});
+        var res = await updateRule({ruleId: this.recordId, agentCode: event.currentTarget.dataset.agentcode});
         if(res) {
-            //error
-            console.log(res);
+            console.error(res);
+            this.showToast('Errore','Si è verificato un errore. Contattare il supporto tecnico.','error');
         } else {
+            this.agents = null;
+            this.template.querySelector(".searchBar").value = null;
             this.connectedCallback();
         }
     }
 
     async clearAgency() {
-        this.setAgent({target: {value: null}});
+        this.setAgent({currentTarget:{dataset:{}}});
+    }
+
+    showToast(title,message,variant = 'info') {
+        this.dispatchEvent(new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+            mode: 'sticky'
+        }));
     }
 }
