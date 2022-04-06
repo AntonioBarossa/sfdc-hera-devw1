6b77d027163a160de41facb861e3e6b6e9bcf5ba
emanalpi <emanalpi@everis.com>
2022-04-06 18:46:59 +0200

    fix defect 2137

--- force-app/main/default/aura/HDT_LCP_CommercialRiassignButton/HDT_LCP_CommercialRiassignButtonController.js
+++ force-app/main/default/aura/HDT_LCP_CommercialRiassignButton/HDT_LCP_CommercialRiassignButtonController.js
@@ -3,5 +3,8 @@
         console.log('refreshPageLOL');
         $A.get('e.force:refreshView').fire();
         window.location.reload();
+    },
+    assignerUpdated : function(component, event, helper){
+        setTimeout(function(){window.location.reload()},1000);
     }
 })
