0d9f97af62d9bafbe23b107f618abc1bb687d6b1
clomele <costanzo.lomele@webresults.it>
2022-05-10 13:32:24 +0200

    fix

--- force-app/main/default/lwc/hdtLandRegistry/hdtLandRegistry.js
+++ force-app/main/default/lwc/hdtLandRegistry/hdtLandRegistry.js
@@ -1,14 +1,14 @@
 import { LightningElement, track, api, wire } from 'lwc';
-import retrieveLandRegistryByServicePointId from '@salesforce/apex/HDT_UTL_LandRegistry.retrieveLandRegistryByServicePointId';
+import retrieveLandRegistriy from '@salesforce/apex/HDT_UTL_LandRegistry.retrieveLandRegistriy';
 
 export default class HdtLandRegistry extends LightningElement {
     
-    @api servicePoint;
-    @api selectedLandRegistry;
+    @api servicePointId;
+    @api selectedLandRegistryId;
     @api required = false;
     @api readonly = false;
 
-    const COLS = [
+    var columns = [
         { label: 'Codice assenza dati catastali', fieldName: 'CodeMissingRegistryData__c', type: 'text' },
         { label: 'Destinazione Uso', fieldName: 'DestinationUsage__c', type: 'text' },
         { label: 'Comune catastale', fieldName: 'RegistryCity__c', type: 'text' },
@@ -21,17 +21,14 @@ export default class HdtLandRegistry extends LightningElement {
         { label: 'Stage', fieldName: 'ParticleSheet__c', type: 'text' },
         { label: 'Stage', fieldName: 'Subaltern__c', type: 'text' },
         { label: 'Stage', fieldName: 'RegistryCategory__c', type: 'text' },
-
         { label: 'Stage', fieldName: 'RegistrySurface__c', type: 'text' },
         // { label: 'Stage', fieldName: 'RegistryCategory__c', type: 'text' }, --> AGG campo mancante!
         { label: 'Stage', fieldName: 'Title__c', type: 'text' }
         
     ];
 
-    connectedCallback() {
-
-
-        retrieveLandRegistryByServicePointId();
+    connectedCallback(){
+        retrieveLandRegistriy();
     }
 
 }
