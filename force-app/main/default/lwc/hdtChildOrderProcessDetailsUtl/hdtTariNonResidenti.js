2c623181de6582f477cc05c86359f17cfe2bf876
Giacinto Florio <giacinto.florio@webresults.it>
2022-07-27 12:15:27 +0200

    Componenti Nucleo Famigliare

--- force-app/main/default/lwc/hdtChildOrderProcessDetailsUtl/hdtTariNonResidenti.js
+++ force-app/main/default/lwc/hdtChildOrderProcessDetailsUtl/hdtTariNonResidenti.js
@@ -8,7 +8,34 @@ class comune {
 
 export const cities = [
     new comune("BORGO TOSSIGNANO", false, ()=>2),
-    new comune("MORINO", false, ()=>2),//test Only, rimuovere
+    new comune("CASALFIUMANESE", false, ()=>2),
+    new comune("CASTEL DEL RIO", false, ()=>2),
+    new comune("CASTEL GUELFO DI BOLOGNA", false, ()=>2),
+    new comune("CASTEL SAN PIETRO TERME", false, ()=>2),
+    new comune("FONTANELICE", false, ()=>2),
+    new comune("IMOLA", false, ()=>2),
+    new comune("MARRADI", true, ()=>3),
+    new comune("MEDICINA", false, ()=>2),
+    new comune("PRIGNANO SULLA SECCHIA", true, ()=>4),
+    new comune("RUSSI", false, ()=>2),
+    new comune("SAN GIORGIO DI PIANO", false, ()=>3),
+    new comune("SERRAMAZZONI", true, ()=>3),
+    new comune("CAMPOGALLIANO", false, (mq) => {
+        if (mq <= 50)   return 1;
+        if (mq <= 80)   return 2;
+        if (mq <= 100)  return 3;
+        if (mq <= 120)  return 4;
+        if (mq <= 140)  return 5;
+        return 6;
+    }),
+    new comune("GRANAROLO DELL'EMILIA", false, (mq) => {
+        if (mq <= 45)   return 1;
+        if (mq <= 60)   return 2;
+        if (mq <= 75)   return 3;
+        if (mq <= 90)   return 4;
+        if (mq <= 105)  return 5;
+        return 6;
+    }),
     new comune("MODENA", false, (mq) => {
         if (mq <= 50)   return 1;
         if (mq <= 80)   return 2;
@@ -17,6 +44,7 @@ export const cities = [
         if (mq <= 140)  return 5;
         return 6;
     })
+
 ]
 .reduce((result, elem)=>{
     result[elem.name] = elem;
