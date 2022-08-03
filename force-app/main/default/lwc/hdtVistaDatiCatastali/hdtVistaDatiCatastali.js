cc6df0999d981ee127c94913d5e61c9c30c54254
clomele <costanzo.lomele@webresults.it>
2022-08-03 18:02:48 +0200

    v1 cmp

--- force-app/main/default/lwc/hdtVistaDatiCatastali/hdtVistaDatiCatastali.js
+++ force-app/main/default/lwc/hdtVistaDatiCatastali/hdtVistaDatiCatastali.js
@@ -1,17 +1,51 @@
 import { LightningElement, api } from 'lwc';
-import getVistaDatiCatastali from '@salesforce/apex/HDT_UTL_LandRegistry.getVistaDatiCatastali';
+import getVistaDatiCatastali from '@salesforce/apexContinuation/HDT_UTL_LandRegistry.getVistaDatiCatastali';
 
+const COLUMNS = [
+    { fieldName: 'destinazioneUso', label: 'Destinazione Uso' },
+    { fieldName: 'codiceAssenzaDatiCatastali', label: 'Codice Assenza Dati Catastali' },
+    { fieldName: 'comuneAmministrativo', label: 'Comune Amministrativo' },
+    { fieldName: 'comuneCatastale', label: 'Comune Catastale' },
+    { fieldName: 'codiceComuneCatastale', label: 'Codice Comune Catastale' },
+    { fieldName: 'codiceProvincia', label: 'Codice Provincia' },
+    { fieldName: 'tipoUnita', label: 'Tipo Unità' },
+    { fieldName: 'sezioneUrbana', label: 'Sezione Urbana' },
+    { fieldName: 'foglio', label: 'Foglio' },
+    { fieldName: 'particella', label: 'Particella' },
+    { fieldName: 'subalterno', label: 'Subalterno' },
+    { fieldName: 'qualificaTitolareUtenza', label: 'Qualifica Titolare' },
+    { fieldName: 'superficieCatastale', label: 'Superficie Catastale' },
+    { fieldName: 'categoriaCatastale', label: 'Categoria Catastale' }
+];
 export default class HdtVistaDatiCatastali extends LightningElement {
 
     @api recordId;
 
-    cols = [];
+    cols = COLUMNS;
     rows = [];
+    title = 'Vista Dati Catastali';
 
     connectedCallback(){
         getVistaDatiCatastali({recordId: this.recordId})
             .then(result => {
-                //assegnare rows
+                if(result.response.status == 'success'){
+                    let posizioni = result.response.data?.posizioni;
+                    let index = 0;
+                    let finalRows = [];
+                    posizioni.forEach(curPos => {
+                        let newRow = curPos;
+                        newRow.Id = index;
+                        finalRows.push(newRow);
+                        index++;
+                    })
+                    this.rows = finalRows;
+                    this.title = 'Vista Dati Catastali - ' + result.recordName;
+                    return;
+                }
+                if(result.response.status == 'failed'){
+                    return;
+                }
+                console.error('Cannot read response!');
             })
             .catch(error => console.error(error));
     }
