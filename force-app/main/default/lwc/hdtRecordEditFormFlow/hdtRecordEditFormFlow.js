7a885e4f1e7d65fe13129e2f391d1fc1fe66c19e
valerionania <valerio.antonino.nania@everis.com>
2022-09-15 12:51:02 +0200

    Fix Revisione Incassi

--- force-app/main/default/lwc/hdtRecordEditFormFlow/hdtRecordEditFormFlow.js
+++ force-app/main/default/lwc/hdtRecordEditFormFlow/hdtRecordEditFormFlow.js
@@ -407,6 +407,10 @@ export default class HdtRecordEditFormFlow extends LightningElement {
             }else{
                 this.labelSaveButton  = 'Conferma Pratica';
             }
+        }else if(this.type == 'Promessa di Pagamento Ente'){
+            let canalePagamento2 = this.selector('ChannelOfPayment__c');
+            canalePagamento2.disabled = true;
+            canalePagamento2.value = 'Banca BONIFICO';
         }
     }
 
