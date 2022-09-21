0e704e2af4b0b406f699f6682df02c32da3f0ac4
dmontors <daniele.montorselli@everis.com>
2022-09-21 08:59:27 +0200

    FP9_mailsender

--- force-app/main/default/lwc/hdtMailSender/hdtMailSender.js
+++ force-app/main/default/lwc/hdtMailSender/hdtMailSender.js
@@ -1,18 +1,48 @@
 import { LightningElement } from "lwc";
 
 export default class HdtMNailSender extends LightningElement {
-    bodyMail = 'Ciao Mario Rossi,<br>questa è la prima mail';
+    mailSender = 'saleforceOrg@mail.com';
+    mailReceiver = 'receiver@mail.com';
+    bodyMail = '';
+    temp1 = '';
+    temp2 = '';
+    temp3 = '';
+    options = [];
 
-    get options() {
-        return [
-            { label: 'New', value: 'new' },
-            { label: 'In Progress', value: 'inProgress' },
-            { label: 'Finished', value: 'finished' },
-        ];
+    connectedCallback(){
+        this.temp1 = 'Ciao Mario Rossi,<br><br>questo è il primo template';
+        this.temp2 = 'Ciao Mario Rossi,<br><br>questo è il secondo template';
+        this.temp3 = 'Ciao Mario Rossi,<br><br>questo è il terzo template';
+
+        this.options.push({ label: 'Template 1', value: 'temp1' });
+        this.options.push({ label: 'Template 2', value: 'temp2' });
+        this.options.push({ label: 'Template 3', value: 'temp3' });
+
     }
 
     handleTemplateChange(event) {
-        this.value = event.detail.value;
+        
+        switch (event.detail.value) {
+            case 'temp1':
+                this.bodyMail = this.temp1;
+                break;
+            case 'temp2':
+                this.bodyMail = this.temp2;
+                break;
+            case 'temp3':
+                this.bodyMail = this.temp3;
+        }
+
+        this.template.querySelectorAll('button').forEach(c => {
+            if(c.name === 'sendComunication'){
+                if(this.bodyMail != undefined && this.bodyMail != ''){
+                    c.removeAttribute('disabled'); 
+                } else {
+                    c.setAttribute('disabled', '');
+                }
+            }
+        });
+
     }
 
     handleChange(event) {
@@ -20,6 +50,6 @@ export default class HdtMNailSender extends LightningElement {
     }
 
     handleClick(event){
-        
+        console.log('>>> send this: ' + this.bodyMail);
     }
 }
