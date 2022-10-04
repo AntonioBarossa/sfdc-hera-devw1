51f2ec0e3c48061ed63017822b77f9bb86d56642
fdefelice <federico.defelice@webresults.it>
2022-10-04 21:52:13 +0200

    ritiro area privata

--- force-app/main/default/lwc/hdtWasteDisposalHergo/hdtWasteDisposalHergo.js
+++ force-app/main/default/lwc/hdtWasteDisposalHergo/hdtWasteDisposalHergo.js
@@ -34,22 +34,24 @@ export default class HdtRecordEditFormFlowAdvanced extends HdtRecordEditFormFlow
     _checkOnLoadHergo = false;
     disableMaterialButton = true;
     _recentWithdrawal = false;
+    _withdrawalFee=false;
+
+    get privateAreaPaid(){
+        return this._checkResidente? this._withdrawConfiguration?.PayIfIsPrivateAreaDom__c : this._withdrawConfiguration?.PayIfIsPrivateAreaNotDom__c;
+    }
 
     virtualChange(event){
         console.log(" ### event -> " + event);
 
-        if(event.target.fieldName == 'ClientTypology__c'){
-            this._checkResidente = event.target.value == 'Domestico';
+        if(!["ClientTypology__c", "TypeOperation__c", "WithdrawalPrivateArea__c"].includes(event.target.fieldName)){
+            return;
         }
 
-        if(event.target.fieldName == 'TypeOperation__c'){
-            this._typeOperation = event.target.value;
-        }
 
-        if(event.target.fieldName == 'ClientTypology__c' || event.target.fieldName == 'TypeOperation__c'){
-            this.template.querySelector("[data-id='WithdrawalFee__c']").value = false;
-        }
-
+        this.template.querySelector("[data-id='WithdrawalFee__c']").value = false;
+        if(event.target.fieldName == 'ClientTypology__c') this._checkResidente = event.target.value == 'Domestico';
+        if(event.target.fieldName == 'TypeOperation__c') this._typeOperation = event.target.value;
+        //if(event.target.fieldName == 'WithdrawalPrivateArea__c' && !event.target.value) 
         this.checkConfiguration(event);
         
     }
@@ -74,54 +76,70 @@ export default class HdtRecordEditFormFlowAdvanced extends HdtRecordEditFormFlow
             return;
         }
 
-        this._withdrawConfiguration = null;
-        this.freeWithdrawConfig.forEach((currentItem)=>{
-            if(currentItem.TypeOperation__c?.includes(this._typeOperation)){ // multiselect picklist
-                this._withdrawConfiguration = currentItem;
-            }
-        });  
+
+        if(event.target.fieldName == 'TypeOperation__c' || !this._typeOperation){
+            this._withdrawConfiguration = null;
+            this.freeWithdrawConfig.forEach((currentItem)=>{
+                if(currentItem.TypeOperation__c?.includes(this._typeOperation)){ // multiselect picklist
+                    this._withdrawConfiguration = currentItem;
+                }
+            });
+        }
+        
 
         console.log("New Config ")
         console.log(this._withdrawConfiguration);
 
         if(!this._withdrawConfiguration){
-            if(event.target.fieldName == 'ClientTypology__c' || event.target.fieldName == 'TypeOperation__c'){
+            //if(event.target.fieldName == 'ClientTypology__c' || event.target.fieldName == 'TypeOperation__c'){
                 this.showMessage('Attenzione','Non è stata trovata una corrispondenza tra la combinazione Comune / Tipo Intervento e la tabella di Configurazione Ritiri Gratuiti. Aprire segnalazione per notificare la problematica.','error');
                 this.template.querySelector("[data-id='WithdrawalFee__c']").value = true;
-            }
+            //}
+            return;
+        }
+
+        if(this._withdrawConfiguration.FreeWithdrawCalculation__c == 'N'){
+            this.isCubatureLimited = 'N';
+            this.disableMaterialButton = false;
+            this.cubatureLimit=null;
         }else{
+            this.isCubatureLimited = 'Y';
+            this.disableMaterialButton = false;
 
-            if(this._withdrawConfiguration.FreeWithdrawCalculation__c == 'N'){
-                this.isCubatureLimited = 'N';
-                this.disableMaterialButton = false;
-                this.cubatureLimit=null;
+            let n = this._checkResidente? this._withdrawConfiguration.ToBePaidWithinMonthsDomestic__c : this._withdrawConfiguration.ToBePaidWithinMonthsNotDomestic__c;
+            console.log('### DataUltimoRitiro -> ' + this.lastWithdrawDate);
+            var dateSubtracted =  this.getDateSubtracted(new Date(),n);
+            console.log('### DataUltimoRitiro-nMesi -> ' + dateSubtracted);
+
+            if(this.lastWithdrawDate > dateSubtracted){
+                this.showMessage('Attenzione', 'Ritiro a pagamento causa ultimo ritiro più recente di '+ n +' mesi','error');
+                this.template.querySelector("[data-id='WithdrawalFee__c']").value = true;
+                this._recentWithdrawal = true;
+            }else if(("Y" == this.privateAreaPaid)  && this.template.querySelector("[data-id='WithdrawalPrivateArea__c']").value ){//(Y==Y)==true?
+                this.showMessage('Attenzione', 'Ritiro a pagamento se effettuato in area privata', 'error');
+                this.template.querySelector("[data-id='WithdrawalFee__c']").value = true;
+                this._recentWithdrawal = true;
             }else{
-                this.isCubatureLimited = 'Y';
-                this.disableMaterialButton = false;
-
-                let n = this._checkResidente? this._withdrawConfiguration.ToBePaidWithinMonthsDomestic__c : this._withdrawConfiguration.ToBePaidWithinMonthsNotDomestic__c;
-                console.log('### DataUltimoRitiro -> ' + this.lastWithdrawDate);
-                var dateSubtracted =  this.getDateSubtracted(new Date(),n);
-                console.log('### DataUltimoRitiro-nMesi -> ' + dateSubtracted);
-
-                if(this.lastWithdrawDate > dateSubtracted){
-                    this.showMessage('Attenzione', 'Ritiro a pagamento causa ultimo ritiro più recente di '+ n +' mesi','error');
-                    this.template.querySelector("[data-id='WithdrawalFee__c']").value = true;
-                    this._recentWithdrawal = true;
-                }else{
-                    this.cubatureLimit = this._checkResidente? this._withdrawConfiguration.ToPayIfVolumeEqualOrHigherDom__c : this._withdrawConfiguration.ToPayIfVolumeEqualOrHigherNotDom__c; 
-                }
-
+                this.cubatureLimit = this._checkResidente? this._withdrawConfiguration.ToPayIfVolumeEqualOrHigherDom__c : this._withdrawConfiguration.ToPayIfVolumeEqualOrHigherNotDom__c; 
             }
         }
+        
+    }
+
+    setCheckboxRitiro(newValue, paymentMessage){
+        const checkbox = this.template.querySelector("[data-id='WithdrawalFee__c']");
+        if(newValue && !checkbox.value){
+            this.showMessage('Attenzione', paymentMessage,'error');
+        }else if(!newValue && checkbox.value){
+            this.showMessage('Attenzione','Ritiro non più a pagamento per i parametri selezionati','success');
+        }
+        checkbox.value = newValue;
     }
 
     handleClose(event){
         console.log('###Close Event >>> ' + JSON.stringify(event.detail));
-        this.template.querySelector("[data-id='MaterialDescription__c']").value = event.detail.label;
-        if(!this._recentWithdrawal){
-            this.template.querySelector("[data-id='WithdrawalFee__c']").value = event.detail.needPayment;
-        }
+        this.template.querySelector("[data-id='MaterialDescription__c']").value = event.detail.label;  
+        this.template.querySelector("[data-id='WithdrawalFee__c']").value = event.detail.needPayment;
     }
 
 
