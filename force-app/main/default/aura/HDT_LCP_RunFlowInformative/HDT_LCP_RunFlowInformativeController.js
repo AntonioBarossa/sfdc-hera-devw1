1ea7316a6e2579b6c497734ed00553241f0df72d
salvsara <salvsara@emeal.nttdata.com>
2022-10-14 11:14:00 +0200

    Fix creazione Case Informative

--- force-app/main/default/aura/HDT_LCP_RunFlowInformative/HDT_LCP_RunFlowInformativeController.js
+++ force-app/main/default/aura/HDT_LCP_RunFlowInformative/HDT_LCP_RunFlowInformativeController.js
@@ -2,13 +2,18 @@
     doInit : function(component, event, helper) {
         var maximizeButton = document.querySelector('[title="Maximize"]');
         console.log('found maximize '+ maximizeButton);
+        console.log('Account Id : ' + component.get("v.servicePointRecord.Account__c"));
         if (maximizeButton) {
             maximizeButton.click();
         }
         var inputVariables = [];
         var flow = component.find("flowData");
         var flowName = 'HDT_FL_PostSalesMasterDispatch';
-        inputVariables.push({ name : 'AccountId', type : 'String', value : '' });
+        var accId = component.get("v.servicePointRecord.Account__c");
+        if(accId){
+            inputVariables.push({ name : 'AccountId', type : 'String', value : accId });
+        }
+        inputVariables.push({ name : 'InputServicePointId', type : 'String', value : component.get("v.recordId") });
         inputVariables.push({ name : 'ProcessType', type : 'String', value : 'Informative' });
         inputVariables.push({ name : 'RecordTypeName', type : 'String', value : 'HDT_RT_Informative' });
         inputVariables.push({ name : 'Context', type : 'String', value : 'GlobalAction' });
