a7a27b621b1fb685f31e69c53de643ed12d01566
antoninofugazzotto <antonino.fugazzotto@nttdata.com>
2022-10-21 13:34:03 +0200

    fix bp class TK 914861C

--- force-app/main/default/lwc/hdtChildOrderProcessPrecheck/hdtChildOrderProcessPrecheck.js
+++ force-app/main/default/lwc/hdtChildOrderProcessPrecheck/hdtChildOrderProcessPrecheck.js
@@ -677,7 +677,8 @@ export default class hdtChildOrderProcessPrecheck extends LightningElement {
             companyName:companyName,
             externalCustomerId:this.order.Account.FiscalCode__c? this.order.Account.FiscalCode__c : this.order.Account.VATNumber__c,
             secondaryCustomerId:secondaryCustomerId,
-            bpClass:bpClass,
+            //bpClass:bpClass,
+            bpClass:this.order.Account.CustomerMarking__c,
             bpCategory:this.order.Account.Category__c,
             bpType:bpType,
             customerType:"CT0", //da definire campo SF con business            
