c2a06742c5bb73c6a76fd41f5393ccc57588498f
clomele <costanzo.lomele@webresults.it>
2022-11-23 17:23:55 +0100

    fix vista dati catastali

--- force-app/main/default/aura/HDT_LCP_OpenCadastralView/HDT_LCP_OpenCadastralViewHelper.js
+++ force-app/main/default/aura/HDT_LCP_OpenCadastralView/HDT_LCP_OpenCadastralViewHelper.js
@@ -7,18 +7,21 @@
         console.log('>>sObjectName', sObjectName);
         var workspaceAPI = component.find("workspace");
 
-        workspaceAPI.openTab({ url: '/' + recordId })
+        // workspaceAPI.openTab({ url: '/' + recordId })
+        //     .then(function(response) {
+        
+        workspaceAPI.getFocusedTabInfo()
             .then(function(response) {
                 workspaceAPI.openSubtab({
-                    parentTabId: response,
+                    parentTabId: response.parentTabId,
                     pageReference: {
                         type: 'standard__component',
                         attributes: { componentName: 'c__HDT_LCP_OpenCadastralViewLwc' },
                         state: { c__recordid: recordId, c__sobjecttype: sObjectName, c__tab: 'VDC' }
                     }
-                })
+                    })
                     .then(function(subtabId) {
-                        workspaceAPI.setTabLabel({ tabId: subtabId, label: 'Vista Dati Catastali' });
+                        workspaceAPI.setTabLabel({ tabId: subtabId, label: 'Dati Catastali' });
                         workspaceAPI.setTabIcon({ tabId: subtabId, icon: 'custom:custom83', iconAlt: 'Vista Dati Catastali' });
                         $A.get("e.force:closeQuickAction").fire();
                     });
