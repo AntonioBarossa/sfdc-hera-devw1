8e2b88883748764992c1b7660f7d72ccfb685458
fdefelice <federico.defelice@webresults.it>
2022-12-05 15:15:58 +0100

    Correzione sviluppi

--- force-app/main/default/aura/HDT_LCP_OpenOdlAdsSegnalazioni/HDT_LCP_OpenOdlAdsSegnalazioniController.js
+++ force-app/main/default/aura/HDT_LCP_OpenOdlAdsSegnalazioni/HDT_LCP_OpenOdlAdsSegnalazioniController.js
@@ -2,10 +2,7 @@
 
     getRecord: function(component, event, helper) {
 		console.log('# open from quick action #');
-        
         helper.setAccountId(component,event,helper);
-
-        helper.openTabWithSubtab(component, event, helper);
     }
 
 })
