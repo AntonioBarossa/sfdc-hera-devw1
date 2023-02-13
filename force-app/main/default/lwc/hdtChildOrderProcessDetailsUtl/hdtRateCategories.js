5512f8367ec099433cf24ca4b12b44a86f98a56b
SiLonghi <simone.longhi@everis.nttdata.com>
2023-02-13 17:58:19 +0100

    [HRADTR_LSW2] fix

--- force-app/main/default/lwc/hdtChildOrderProcessDetailsUtl/hdtRateCategories.js
+++ force-app/main/default/lwc/hdtChildOrderProcessDetailsUtl/hdtRateCategories.js
@@ -17,7 +17,7 @@
     export const AFUNDC =  { evaluationType: 'visible', rateCategories: ['ACARTCOMM0','ACPROMISC0','ACPROMIBI0'] };
     export const AFUNDI =  { evaluationType: 'visible', rateCategories: ['ACINDSTR00','ACAUGRUNI0','ACPROMIBI0'] };
     export const AFUNDZ =  { evaluationType: 'visible', rateCategories: ['ACZOOTECN0','ACAGRIZOO0'] };
-    export const AFUNAC =  { evaluationType: 'visible', rateCategories: ['ACARTCOMM0','ACAUGRUNC0','ACIPROMIBIO'] };
+    export const AFUNAC =  { evaluationType: 'visible', rateCategories: ['ACARTCOMM0','ACAUGRUNC0','ACPROMIBI0'] };
     export const N_A =  { evaluationType: 'visible', rateCategories: ['ACDOMRESP0'] };
 
     //Rate Category Required
