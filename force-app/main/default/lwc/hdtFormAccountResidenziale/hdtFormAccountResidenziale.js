c9d4f3f350e2ea71a105b0d6f39b7a23384ab661
gtreglia <gabriele.treglia.st@everis.nttdata.com>
2023-07-28 16:22:00 +0200

    [AP] RT e filtri creazione account TARING

--- force-app/main/default/lwc/hdtFormAccountResidenziale/hdtFormAccountResidenziale.js
+++ force-app/main/default/lwc/hdtFormAccountResidenziale/hdtFormAccountResidenziale.js
@@ -152,6 +152,8 @@ export default class HdtFormAccountResidenziale extends NavigationMixin(Lightnin
         //this.filterMarkingOptions(customerMarkingOptionsPreCheck);
         this.companyPicklist( event.target.value);
         this.customerMarkingOptions = this.customerData.values.filter(opt => opt.validFor.includes(key));
+        const tariNgMarkings = ['BP TARI NG AAA','BP TARIP','BP TARI NG MMS']
+        this.customerMarkingOptions = this.customerData.values.filter(opt => (opt.validFor.includes(key) && tariNgMarkings.indexOf(opt.value) === -1 ));
         this.markingValue = '';
         this.categoryValue = '';
         
